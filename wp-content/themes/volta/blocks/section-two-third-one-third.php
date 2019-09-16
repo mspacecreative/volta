e22bd128e329e7a98220e81b4513f697b4ae0f5a
Matt <matt@mspacecreative.com>
2019-09-16 20:52:47 -0300

    c

--- wp-content/themes/volta/blocks/section-two-third-one-third.php
+++ wp-content/themes/volta/blocks/section-two-third-one-third.php
@@ -1,3 +1,31 @@
+<?php if ( get_field('section_padding') == 'top' ): ?>
+<div class="clear top-padding">
+	<div class="two_third_col">
+		<?php the_field('two_third_column'); ?>
+	</div>
+	<div class="one_third_col">
+		<?php the_field('one_third_column'); ?>
+	</div>
+</div>
+<?php elseif ( get_field('section_padding') == 'bottom' ): ?>
+<div class="clear bottom-padding">
+	<div class="two_third_col">
+		<?php the_field('two_third_column'); ?>
+	</div>
+	<div class="one_third_col">
+		<?php the_field('one_third_column'); ?>
+	</div>
+</div>
+<?php elseif ( get_field('section_padding') == 'both' ): ?>
+<div class="clear top-bottom-padding">
+	<div class="two_third_col">
+		<?php the_field('two_third_column'); ?>
+	</div>
+	<div class="one_third_col">
+		<?php the_field('one_third_column'); ?>
+	</div>
+</div>
+<?php else : ?>
 <div class="clear">
 	<div class="two_third_col">
 		<?php the_field('two_third_column'); ?>
@@ -6,3 +34,4 @@
 		<?php the_field('one_third_column'); ?>
 	</div>
 </div>
+<?php endif; ?>
