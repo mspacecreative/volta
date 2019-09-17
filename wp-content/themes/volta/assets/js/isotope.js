f0c20fa8581fa079b2024604812297dde7118200
Matt <matt@mspacecreative.com>
2019-09-17 17:23:35 -0300

    c

--- wp-content/themes/volta/assets/js/isotope.js
+++ wp-content/themes/volta/assets/js/isotope.js
@@ -1,16 +1,14 @@
 (function ($) {
 	    
-	var grid = $('.iso-grid').isotope({
-		itemSelector: ".grid-item",
-		percentPosition: true,
-		masonry: {
-		    columnWidth: '.grid-sizer',
-			gutter: '.gutter-sizer',
-		}
+	$('.iso-grid').imagesLoaded( function() {
+		$('.iso-grid').isotope({
+			itemSelector: ".grid-item",
+			percentPosition: true,
+			masonry: {
+			    columnWidth: '.grid-sizer',
+				gutter: '.gutter-sizer',
+			}
+		});
 	});
-		
-	grid.imagesLoaded().progress( function() {
-		grid.isotope('layout');
-    });
 	
 })(jQuery);
