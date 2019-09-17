b4ab7e1c70c780a02690d33b9781fb72283b8312
Matt <matt@mspacecreative.com>
2019-09-17 15:36:42 -0300

    c

--- wp-content/themes/volta/author.php
+++ wp-content/themes/volta/author.php
@@ -1,22 +1,22 @@
 <?php get_header(); ?>
 
 <!-- Page wrapper -->
-<div id="page-wrapper">
+<div id="page-wrapper" class="blog">
 	
 	<!-- post thumbnail -->
 	<?php
-	if ( get_field('featured_image_alignment') == 'top' ): ?>
-	<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-	echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: top center;"></div>'; ?>
-	<?php elseif ( get_field('featured_image_alignment') == 'bottom' ): ?>
-	<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+	if ( get_field('featured_image_alignment', 'options') == 'top' ): ?>
+	<?php $url = get_field('featured_image_blog', 'options' ); ?>
+	<?php echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: top center;"></div>'; ?>
+	<?php elseif ( get_field('featured_image_alignment', 'options') == 'bottom' ): ?>
+	<?php $url = get_field('featured_image_blog', 'options' ); ?>
 	<?php echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: bottom center;"></div>'; ?>
-	<?php elseif ( get_field('featured_image_alignment') == 'center' ): ?>
-	<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-	echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: center;"></div>'; ?>
+	<?php elseif ( get_field('featured_image_alignment', 'options') == 'center' ): ?>
+	<?php $url = get_field('featured_image_blog', 'options' ); ?>
+	<?php echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: center;"></div>'; ?>
 	<?php else : ?>
-	<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-	echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: center;"></div>';
+	<?php $url = echo get_template_directory_uri() . '/img/header.jpg'; ?>
+	<?php echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: center;"></div>';
 	endif; ?>
 	<!-- /post thumbnail -->
 	
@@ -25,8 +25,11 @@
 		<main role="main">
 			<!-- section -->
 			<section>
-	
-				<h1><?php _e( 'Author Archives for ', 'html5blank' ); echo get_the_author(); ?></h1>
+				
+				<!-- post title -->
+				<div class="post-title-container">
+					<h1><?php _e( 'Author Archives for ', 'html5blank' ); echo get_the_author(); ?></h1>
+				</div>
 				
 				<?php if ( get_the_author_meta('description')) : ?>
 				
@@ -37,16 +40,17 @@
 					<?php echo wpautop( get_the_author_meta('description') ); ?>
 				
 				<?php endif; ?>
-	
-				<?php get_template_part('loop'); ?>
-	
-				<?php get_template_part('pagination'); ?>
+				<!-- /post title -->
+					
+				<div class="inner">
+					<?php get_template_part('loop'); ?>
+				
+					<?php get_template_part('pagination'); ?>
+				</div>
 	
 			</section>
 			<!-- /section -->
 		</main>
-
-		<?php get_sidebar(); ?>
 
 	</div>
 	
