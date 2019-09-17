e6c790da5188b1e69370648e9adae8bf2c8448be
Matt <matt@mspacecreative.com>
2019-09-17 18:23:29 -0300

    c

--- wp-content/themes/volta/blocks/section-flexbox.php
+++ wp-content/themes/volta/blocks/section-flexbox.php
@@ -218,6 +218,82 @@
 	<?php if ( get_field('section_heading') ): ?>
 	<h2><?php the_field('section_heading'); ?></h2>
 	<?php endif; ?>
+	<?php if ( get_field('column_count') == 'two' ): ?>
+	<?php if( have_rows('flexbox') ): ?>
+	<div class="display-flex clear">
+		<?php while( have_rows('flexbox') ): the_row(); ?>
+		<div class="flexbox-2">
+			<div>
+				<?php if( have_rows('flexbox_content') ): ?>
+				<?php while( have_rows('flexbox_content') ): the_row(); ?>
+				
+				<h3><?php the_sub_field('heading'); ?></h3>
+				<p><?php the_sub_field('body'); ?></p>
+				
+				<?php if( have_rows('button') ): ?>
+				<?php while( have_rows('button') ): the_row(); ?>
+				<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+				<?php endwhile; ?>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
+	<?php elseif ( get_field('column_count') == 'three' ): ?>
+	<?php if( have_rows('flexbox') ): ?>
+	<div class="display-flex clear">
+		<?php while( have_rows('flexbox') ): the_row(); ?>
+		<div class="flexbox-3">
+			<div>
+				<?php if( have_rows('flexbox_content') ): ?>
+				<?php while( have_rows('flexbox_content') ): the_row(); ?>
+				
+				<h3><?php the_sub_field('heading'); ?></h3>
+				<p><?php the_sub_field('body'); ?></p>
+				
+				<?php if( have_rows('button') ): ?>
+				<?php while( have_rows('button') ): the_row(); ?>
+				<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+				<?php endwhile; ?>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
+	<?php elseif ( get_field('column_count') == 'four' ): ?>
+	<?php if( have_rows('flexbox') ): ?>
+	<div class="display-flex clear">
+		<?php while( have_rows('flexbox') ): the_row(); ?>
+		<div class="flexbox-4">
+			<div>
+				<?php if( have_rows('flexbox_content') ): ?>
+				<?php while( have_rows('flexbox_content') ): the_row(); ?>
+				
+				<h3><?php the_sub_field('heading'); ?></h3>
+				<p><?php the_sub_field('body'); ?></p>
+				
+				<?php if( have_rows('button') ): ?>
+				<?php while( have_rows('button') ): the_row(); ?>
+				<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+				<?php endwhile; ?>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
+	<?php elseif ( get_field('column_count') == 'five' ): ?>
 	<?php if( have_rows('flexbox') ): ?>
 	<div class="display-flex clear">
 		<?php while( have_rows('flexbox') ): the_row(); ?>
@@ -242,11 +318,88 @@
 		<?php endwhile; ?>
 	</div>
 	<?php endif; ?>
+	<?php endif; ?>
 </div>
 <?php else : ?>
 <?php if ( get_field('section_heading') ): ?>
 <h2><?php the_field('section_heading'); ?></h2>
 <?php endif; ?>
+<?php if ( get_field('column_count') == 'two' ): ?>
+<?php if( have_rows('flexbox') ): ?>
+<div class="display-flex clear">
+	<?php while( have_rows('flexbox') ): the_row(); ?>
+	<div class="flexbox-2">
+		<div>
+			<?php if( have_rows('flexbox_content') ): ?>
+			<?php while( have_rows('flexbox_content') ): the_row(); ?>
+			
+			<h3><?php the_sub_field('heading'); ?></h3>
+			<p><?php the_sub_field('body'); ?></p>
+			
+			<?php if( have_rows('button') ): ?>
+			<?php while( have_rows('button') ): the_row(); ?>
+			<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+			<?php endwhile; ?>
+			<?php endif; ?>
+			
+			<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
+	<?php endwhile; ?>
+</div>
+<?php endif; ?>
+<?php elseif ( get_field('column_count') == 'three' ): ?>
+<?php if( have_rows('flexbox') ): ?>
+<div class="display-flex clear">
+	<?php while( have_rows('flexbox') ): the_row(); ?>
+	<div class="flexbox-3">
+		<div>
+			<?php if( have_rows('flexbox_content') ): ?>
+			<?php while( have_rows('flexbox_content') ): the_row(); ?>
+			
+			<h3><?php the_sub_field('heading'); ?></h3>
+			<p><?php the_sub_field('body'); ?></p>
+			
+			<?php if( have_rows('button') ): ?>
+			<?php while( have_rows('button') ): the_row(); ?>
+			<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+			<?php endwhile; ?>
+			<?php endif; ?>
+			
+			<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
+	<?php endwhile; ?>
+</div>
+<?php endif; ?>
+<?php elseif ( get_field('column_count') == 'four' ): ?>
+<?php if( have_rows('flexbox') ): ?>
+<div class="display-flex clear">
+	<?php while( have_rows('flexbox') ): the_row(); ?>
+	<div class="flexbox-4">
+		<div>
+			<?php if( have_rows('flexbox_content') ): ?>
+			<?php while( have_rows('flexbox_content') ): the_row(); ?>
+			
+			<h3><?php the_sub_field('heading'); ?></h3>
+			<p><?php the_sub_field('body'); ?></p>
+			
+			<?php if( have_rows('button') ): ?>
+			<?php while( have_rows('button') ): the_row(); ?>
+			<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+			<?php endwhile; ?>
+			<?php endif; ?>
+			
+			<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
+	<?php endwhile; ?>
+</div>
+<?php endif; ?>
+<?php elseif ( get_field('column_count') == 'five' ): ?>
 <?php if( have_rows('flexbox') ): ?>
 <div class="display-flex clear">
 	<?php while( have_rows('flexbox') ): the_row(); ?>
