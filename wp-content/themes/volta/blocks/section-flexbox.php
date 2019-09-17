3307c91b638809a30572b7c3acb08b0128cd715e
Matt <matt@mspacecreative.com>
2019-09-17 10:51:11 -0300

    c

--- wp-content/themes/volta/blocks/section-flexbox.php
+++ wp-content/themes/volta/blocks/section-flexbox.php
@@ -1,3 +1,95 @@
+<?php if ( get_field('section_padding') == 'top' ): ?>
+<div class="top-padding">
+	<?php if ( get_field('section_heading') ): ?>
+	<h2><?php the_field('section_heading'); ?></h2>
+	<?php endif; ?>
+	<?php if( have_rows('flexbox') ): ?>
+	<div class="display-flex clear">
+		<?php while( have_rows('flexbox') ): the_row(); ?>
+		<div class="flexbox-5">
+			<div>
+				<?php if( have_rows('flexbox_content') ): ?>
+				<?php while( have_rows('flexbox_content') ): the_row(); ?>
+				
+				<h3><?php the_sub_field('heading'); ?></h3>
+				<p><?php the_sub_field('body'); ?></p>
+				
+				<?php if( have_rows('button') ): ?>
+				<?php while( have_rows('button') ): the_row(); ?>
+				<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+				<?php endwhile; ?>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
+</div>
+
+<?php elseif ( get_field('section_padding') == 'bottom' ): ?>
+<div class="bottom-padding">
+	<?php if ( get_field('section_heading') ): ?>
+	<h2><?php the_field('section_heading'); ?></h2>
+	<?php endif; ?>
+	<?php if( have_rows('flexbox') ): ?>
+	<div class="display-flex clear">
+		<?php while( have_rows('flexbox') ): the_row(); ?>
+		<div class="flexbox-5">
+			<div>
+				<?php if( have_rows('flexbox_content') ): ?>
+				<?php while( have_rows('flexbox_content') ): the_row(); ?>
+				
+				<h3><?php the_sub_field('heading'); ?></h3>
+				<p><?php the_sub_field('body'); ?></p>
+				
+				<?php if( have_rows('button') ): ?>
+				<?php while( have_rows('button') ): the_row(); ?>
+				<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+				<?php endwhile; ?>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
+</div>
+<?php elseif ( get_field('section_padding') == 'both' ): ?>
+<div class="top-bottom-padding">
+	<?php if ( get_field('section_heading') ): ?>
+	<h2><?php the_field('section_heading'); ?></h2>
+	<?php endif; ?>
+	<?php if( have_rows('flexbox') ): ?>
+	<div class="display-flex clear">
+		<?php while( have_rows('flexbox') ): the_row(); ?>
+		<div class="flexbox-5">
+			<div>
+				<?php if( have_rows('flexbox_content') ): ?>
+				<?php while( have_rows('flexbox_content') ): the_row(); ?>
+				
+				<h3><?php the_sub_field('heading'); ?></h3>
+				<p><?php the_sub_field('body'); ?></p>
+				
+				<?php if( have_rows('button') ): ?>
+				<?php while( have_rows('button') ): the_row(); ?>
+				<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+				<?php endwhile; ?>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
+</div>
+<?php else : ?>
 <?php if ( get_field('section_heading') ): ?>
 <h2><?php the_field('section_heading'); ?></h2>
 <?php endif; ?>
@@ -24,4 +116,5 @@
 	</div>
 	<?php endwhile; ?>
 </div>
+<?php endif; ?>
 <?php endif; ?>
