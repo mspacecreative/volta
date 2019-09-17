d76f9bdb289f4da9b2b71b0a2b7b5182ed9a32cb
Matt <matt@mspacecreative.com>
2019-09-17 15:47:39 -0300

    c

--- wp-content/themes/volta/includes/loops/loop.php
+++ wp-content/themes/volta/includes/loops/loop.php
@@ -1,20 +1,17 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" <?php post_class('clear'); ?>>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
 		<!-- post thumbnail -->
-		<div class="one-third">
 		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
 			</a>
 		<?php endif; ?>
-		</div>
 		<!-- /post thumbnail -->
 	
 		<!-- post title -->
-		<div class="two-third">
 		<h2>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 		</h2>
@@ -29,7 +26,6 @@
 		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
 	
 		<?php edit_post_link(); ?>
-		</div>
 	
 	</article>
 	<!-- /article -->
