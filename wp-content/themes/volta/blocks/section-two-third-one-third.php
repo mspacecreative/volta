71999842ed46262e46e55a0fd2f7505ae6b915d0
Matt <matt@mspacecreative.com>
2019-09-17 20:06:50 -0300

    c

--- wp-content/themes/volta/blocks/section-two-third-one-third.php
+++ wp-content/themes/volta/blocks/section-two-third-one-third.php
@@ -70,6 +70,7 @@
 	<?php endif; ?>
 	
 </div>
+
 <?php elseif ( get_field('section_padding') == 'both' ): ?>
 <div class="clear top-bottom-padding">
 	<div class="two_third_col">
@@ -106,6 +107,44 @@
 	<?php endif; ?>
 	
 </div>
+
+<?php elseif ( get_field('section_padding') == 'full' ): ?>
+<div class="clear full-padding">
+	<div class="two_third_col">
+		<?php the_field('two_third_column'); ?>
+	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php 
+			$image = get_sub_field('image_upload');
+			$size = 'large';
+			
+			if( $image ) {
+				echo wp_get_attachment_image( $image, $size );
+			}
+			?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
+</div>
+
 <?php else : ?>
 <div class="clear">
 	<div class="two_third_col">
