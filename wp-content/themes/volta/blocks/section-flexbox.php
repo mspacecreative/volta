631ac890800e1001c0abb6ba8d50ccd4fda2884f
Matt <matt@mspacecreative.com>
2019-09-16 21:14:51 -0300

    c

--- wp-content/themes/volta/blocks/section-flexbox.php
+++ wp-content/themes/volta/blocks/section-flexbox.php
@@ -2,7 +2,14 @@
 <div class="display-flex clear">
 	<?php while( have_rows('flexbox') ): the_row(); ?>
 	<div class="flexbox">
-		<?php the_sub_field('flexbox_content'); ?>
+		<?php if( have_rows('flexbox_content') ): ?>
+		<?php while( have_rows('flexbox_content') ): the_row(); ?>
+		<h3><?php the_sub_field('heading'); ?></h3>
+		<p><?php the_sub_field('body'); ?></p>
+		<?php if( have_rows('button') ): ?>
+		<?php while( have_rows('button') ): the_row(); ?>
+		<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+		<?php endif; ?>
 	</div>
 	<?php endwhile; ?>
 </div>
