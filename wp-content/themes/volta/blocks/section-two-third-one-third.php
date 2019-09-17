a9ceaf2ca802bd64a8d2f9d06c2fd5cf14929991
Matt <matt@mspacecreative.com>
2019-09-17 19:14:43 -0300

    c

--- wp-content/themes/volta/blocks/section-two-third-one-third.php
+++ wp-content/themes/volta/blocks/section-two-third-one-third.php
@@ -3,35 +3,115 @@
 	<div class="two_third_col">
 		<?php the_field('two_third_column'); ?>
 	</div>
-	<div class="one_third_col">
-		<?php the_field('one_third_column'); ?>
-	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php the_field('image_upload'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
 </div>
 <?php elseif ( get_field('section_padding') == 'bottom' ): ?>
 <div class="clear bottom-padding">
 	<div class="two_third_col">
 		<?php the_field('two_third_column'); ?>
 	</div>
-	<div class="one_third_col">
-		<?php the_field('one_third_column'); ?>
-	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php the_field('image_upload'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
 </div>
 <?php elseif ( get_field('section_padding') == 'both' ): ?>
 <div class="clear top-bottom-padding">
 	<div class="two_third_col">
 		<?php the_field('two_third_column'); ?>
 	</div>
-	<div class="one_third_col">
-		<?php the_field('one_third_column'); ?>
-	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php the_field('image_upload'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
 </div>
 <?php else : ?>
 <div class="clear">
 	<div class="two_third_col">
 		<?php the_field('two_third_column'); ?>
 	</div>
-	<div class="one_third_col">
-		<?php the_field('one_third_column'); ?>
-	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php the_field('image_upload'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
 </div>
 <?php endif; ?>
