a3e2e1ef88e4b2755a264845b095774c6c65ac02
Matt <matt@mspacecreative.com>
2019-09-16 21:27:05 -0300

    c

--- wp-content/themes/volta/blocks/section-flexbox.php
+++ wp-content/themes/volta/blocks/section-flexbox.php
@@ -1,21 +1,23 @@
 <?php if( have_rows('flexbox') ): ?>
 <div class="display-flex clear">
 	<?php while( have_rows('flexbox') ): the_row(); ?>
-	<div class="flexbox">
-		<?php if( have_rows('flexbox_content') ): ?>
-		<?php while( have_rows('flexbox_content') ): the_row(); ?>
-		
-		<h3><?php the_sub_field('heading'); ?></h3>
-		<p><?php the_sub_field('body'); ?></p>
-		
-		<?php if( have_rows('button') ): ?>
-		<?php while( have_rows('button') ): the_row(); ?>
-		<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
-		<?php endwhile; ?>
-		<?php endif; ?>
-		
-		<?php endwhile; ?>
-		<?php endif; ?>
+	<div class="flexbox-5">
+		<div>
+			<?php if( have_rows('flexbox_content') ): ?>
+			<?php while( have_rows('flexbox_content') ): the_row(); ?>
+			
+			<h3><?php the_sub_field('heading'); ?></h3>
+			<p><?php the_sub_field('body'); ?></p>
+			
+			<?php if( have_rows('button') ): ?>
+			<?php while( have_rows('button') ): the_row(); ?>
+			<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+			<?php endwhile; ?>
+			<?php endif; ?>
+			
+			<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
 	</div>
 	<?php endwhile; ?>
 </div>
