483a7ef0d623bb716e5db2575692f345cc77f805
Matt <matt@mspacecreative.com>
2019-09-18 08:59:53 -0300

    c

--- wp-content/themes/volta/blocks/section-cta.php
+++ wp-content/themes/volta/blocks/section-cta.php
@@ -19,7 +19,7 @@
 		<footer>
 			
 			<ul class="actions center-items">
-				<li><a href="<?php the_sub_field('button_link'); ?>" class="light button"><?php the_sub_field('button_label'); ?></a></li>
+				<li><a href="<?php the_sub_field('button_link'); ?>" class="dark button"><?php the_sub_field('button_label'); ?></a></li>
 			</ul>
 			
 		</footer>
@@ -63,7 +63,7 @@
 		<footer>
 			
 			<ul class="actions center-items">
-				<li><a href="<?php the_sub_field('button_link'); ?>" class="light button"><?php the_sub_field('button_label'); ?></a></li>
+				<li><a href="<?php the_sub_field('button_link'); ?>" class="dark button"><?php the_sub_field('button_label'); ?></a></li>
 			</ul>
 			
 		</footer>
