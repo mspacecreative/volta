6366d58416ade2f72c3b4948b53090a48ef34cef
Matt <matt@mspacecreative.com>
2019-09-18 12:36:25 -0300

    c

--- wp-content/themes/volta/blocks/section-cta.php
+++ wp-content/themes/volta/blocks/section-cta.php
@@ -66,36 +66,6 @@
 		<?php endif; ?>
 		
 	</header>
-	<?php else : ?>
-	<header class="max-width-800">
-		
-		<?php if ( get_field('heading_size') == 'large' ): ?>
-		<h1><?php the_field('heading'); ?></h1>
-		<?php elseif ( get_field('heading_size') == 'small' ): ?>
-		<h2><?php the_field('heading'); ?></h2>
-		<?php endif; ?>
-		
-		<?php if ( get_field('paragraph_size') == 'small' ): ?>
-		<p><?php the_field('content'); ?></p>
-		<?php elseif ( get_field('paragraph_size') == 'large' ): ?>
-		<p class="large"><?php the_field('content'); ?></p>
-		<?php endif; ?>
-		
-		<?php if( have_rows('cta_button') ): ?>
-		<?php while( have_rows('cta_button') ): the_row(); ?>
-		<?php if ( get_sub_field('button_link') ): ?>
-		<footer>
-			
-			<ul class="actions center-items">
-				<li><a href="<?php the_sub_field('button_link'); ?>" class="dark button"><?php the_sub_field('button_label'); ?></a></li>
-			</ul>
-			
-		</footer>
-		<?php endif; ?>
-		<?php endwhile; ?>
-		<?php endif; ?>
-		
-	</header>
 	<?php endif; ?>
 </section>
 <!-- /Banner -->
