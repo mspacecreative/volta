c5c1b653dee7b6b97a3c82bdef11832114b7b2b2
Matt <matt@mspacecreative.com>
2019-09-18 07:55:07 -0300

    c

--- wp-content/themes/volta/page-custom.php
+++ wp-content/themes/volta/page-custom.php
@@ -43,18 +43,16 @@ get_header(); ?>
 				</div>
 			</div>
 			<!-- /post title -->
-			<div class="inner-padding">
-				<?php 
-				if ( have_posts() ) {
-					while ( have_posts() ) {
-						the_post(); 
-						//
-						the_content();
-						//
-					} // end while
-				} // end if
-				?>
-			</div>
+			<?php 
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post(); 
+					//
+					the_content();
+					//
+				} // end while
+			} // end if
+			?>
 		</div>
 	</div>
 
