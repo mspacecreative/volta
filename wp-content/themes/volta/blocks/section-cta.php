bc228dbf6ab72b9db70da7c2b949c75312dc21f1
Matt <matt@mspacecreative.com>
2019-09-18 13:58:24 -0300

    c

--- wp-content/themes/volta/blocks/section-cta.php
+++ wp-content/themes/volta/blocks/section-cta.php
@@ -9,24 +9,38 @@
 	<?php if ( get_field('text_color') == 'dark' ): ?>
 	<header class="max-width-800">
 		
-		<?php if ( get_field('heading_size') == 'large' ): ?>
+		<?php
+		$headingsize = get_field('heading_size');
+		
+		if ( $headingsize == 'large' ): ?>
+			
 			<?php if ( get_field('heading') ): ?>
 			<h1><?php the_field('heading'); ?></h1>
 			<?php endif; ?>
-		<?php elseif ( get_field('heading_size') == 'small' ): ?>
+		
+		<?php elseif ( $headingsize == 'small' ): ?>
+		
 			<?php if ( get_field('heading') ): ?>
 			<h2><?php the_field('heading'); ?></h2>
 			<?php endif; ?>
+			
 		<?php endif; ?>
 		
-		<?php if ( get_field('paragraph_size') == 'large' ): ?>
+		<?php 
+		$bodysize = get_field('paragraph_size');
+		
+		if ( $bodysize == 'large' ): ?>
+		
 			<?php if ( get_field('content') ): ?>
 			<p class="large"><?php the_field('content'); ?></p>
 			<?php endif; ?>
-		<?php elseif ( get_field('paragraph_size') == 'small' ): ?>
+			
+		<?php elseif ( $bodysize == 'small' ): ?>
+		
 			<?php if ( get_field('content') ): ?>
 			<p><?php the_field('content'); ?></p>
 			<?php endif; ?>
+			
 		<?php endif; ?>
 		
 		<?php if( have_rows('cta_button') ): ?>
@@ -47,16 +61,38 @@
 	<?php elseif ( get_field('text_color') == 'light' ): ?>
 	<header class="light max-width-800">
 		
-		<?php if ( get_field('heading_size') == 'large' ): ?>
-		<h1><?php the_field('heading'); ?></h1>
-		<?php elseif ( get_field('heading_size') == 'small' ): ?>
-		<h2><?php the_field('heading'); ?></h2>
+		<?php
+		$headingsize = get_field('heading_size');
+		
+		if ( $headingsize == 'large' ): ?>
+			
+			<?php if ( get_field('heading') ): ?>
+			<h1><?php the_field('heading'); ?></h1>
+			<?php endif; ?>
+		
+		<?php elseif ( $headingsize == 'small' ): ?>
+		
+			<?php if ( get_field('heading') ): ?>
+			<h2><?php the_field('heading'); ?></h2>
+			<?php endif; ?>
+			
 		<?php endif; ?>
 		
-		<?php if ( get_field('paragraph_size') == 'large' ): ?>
-		<p class="large"><?php the_field('content'); ?></p>
-		<?php elseif ( get_field('paragraph_size') == 'small' ): ?>
-		<p><?php the_field('content'); ?></p>
+		<?php 
+		$bodysize = get_field('paragraph_size');
+		
+		if ( $bodysize == 'large' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<p class="large"><?php the_field('content'); ?></p>
+			<?php endif; ?>
+			
+		<?php elseif ( $bodysize == 'small' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<p><?php the_field('content'); ?></p>
+			<?php endif; ?>
+			
 		<?php endif; ?>
 		
 		<?php if( have_rows('cta_button') ): ?>
