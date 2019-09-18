716a6bfaeb6aff24b582cbdd88ba8e5196c2dbea
Matt <matt@mspacecreative.com>
2019-09-18 10:54:33 -0300

    c

--- wp-content/themes/volta/blocks/section-two-third-one-third.php
+++ wp-content/themes/volta/blocks/section-two-third-one-third.php
@@ -112,43 +112,6 @@ if ( $padding == 'top' && $margin == 'top' ): ?>
 	
 </div>
 
-<?php elseif ( $padding == 'full' ): ?>
-<div class="clear full-padding">
-	<div class="two_third_col">
-		<?php the_field('two_third_column'); ?>
-	</div>
-	
-	<?php if( have_rows('one_third_column') ): ?>
-	<?php while( have_rows('one_third_column') ): the_row(); ?>
-	
-		<?php if ( get_sub_field('content_type') == 'text' ): ?>
-		<div class="one_third_col">
-			<?php the_sub_field('text_editor'); ?>
-		</div>
-		
-		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
-		<div class="one_third_col">
-			<?php 
-			$image = get_sub_field('image_upload');
-			$size = 'large';
-			
-			if( $image ) {
-				echo wp_get_attachment_image( $image, $size );
-			}
-			?>
-		</div>
-		
-		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
-		<div class="one_third_col">
-			<?php the_sub_field('image_upload'); ?>
-		</div>
-		<?php endif; ?>
-	
-	<?php endwhile; ?>
-	<?php endif; ?>
-	
-</div>
-
 <?php elseif ( $padding == 'top' && $margin == 'bottom' ): ?>
 <div class="clear top-padding bottom-margin">
 	<div class="two_third_col">
