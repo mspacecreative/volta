e9bc4984b5fde152d2e502d754f275af362606c8
Matt <matt@mspacecreative.com>
2019-09-19 12:21:38 -0300

    c

--- wp-content/themes/volta/category.php
+++ wp-content/themes/volta/category.php
@@ -1,19 +1,65 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<!-- Background image -->
+<div class="background">
+	<div class="gradient-overlay"></div>
+</div>
+<!-- /Background image -->
 
-			<h1><?php _e( 'Categories for ', 'html5blank' ); single_cat_title(); ?></h1>
+<!-- Page wrapper -->
+<div id="page-wrapper" class="blog">
+	
+	<!-- post thumbnail -->
+	<?php
+	if ( get_field('featured_image_alignment') == 'top' ): ?>
+	<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+	echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: top center;"></div>'; ?>
+	<?php elseif ( get_field('featured_image_alignment') == 'bottom' ): ?>
+	<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+	<?php echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: bottom center;"></div>'; ?>
+	<?php elseif ( get_field('featured_image_alignment') == 'center' ): ?>
+	<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+	echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: center;"></div>'; ?>
+	<?php else : ?>
+	<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+	echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: center;"></div>';
+	endif; ?>
+	<!-- /post thumbnail -->
+	
+	<!-- post thumbnail on mobile -->
+	<div class="featured-image-mobile">
+		<img src="<?php echo get_template_directory_uri(); ?>/img/header-mobile.jpg">
+	</div>
+	<!-- /post thumbnail on mobile -->
+	
+	<!-- Nav -->
+	<nav id="nav">
+		<?php html5blank_nav(); ?>
+	</nav>
+	<!-- /Nav -->
+	
+	<div class="post-content-container container white-bg clear">
 
-			<?php get_template_part('loop'); ?>
+		<main role="main">
+			<!-- section -->
+			<section>
+				
+				<!-- post title -->
+				<div class="post-title-container">
+					<h1><?php _e( 'Categories for ', 'html5blank' ); single_cat_title(); ?></h1>
+				</div>
+				<!-- /post title -->
+	
+				<?php get_template_part('loop-masonry'); ?>
+	
+				<?php get_template_part('pagination'); ?>
+	
+			</section>
+			<!-- /section -->
+		</main>
 
-			<?php get_template_part('pagination'); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	</div>
+	
+</div>
 
 <?php get_footer(); ?>
