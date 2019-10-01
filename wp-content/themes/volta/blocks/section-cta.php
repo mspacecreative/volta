a5b24e0e7028c3b54dbf0318616637a149772e63
Matt <matt@mspacecreative.com>
2019-10-01 10:22:35 -0300

    c

--- wp-content/themes/volta/blocks/section-cta.php
+++ wp-content/themes/volta/blocks/section-cta.php
@@ -6,18 +6,441 @@ $backgroundcolour = get_field('background_color');
 if ( $imageoverlay == 'dark' && $backgroundimage ): ?>
 <section class="banner dark-overlay" style="background-image: url(<?php the_field('background_image'); ?>);">
 
+	<?php if ( get_field('text_color') == 'dark' ): ?>
+	<header class="max-width-800">
+		
+		<?php
+		$headingsize = get_field('heading_size');
+		
+		if ( $headingsize == 'large' ): ?>
+			
+			<?php if ( get_field('heading') ): ?>
+			<h1><?php the_field('heading'); ?></h1>
+			<?php endif; ?>
+		
+		<?php elseif ( $headingsize == 'small' ): ?>
+		
+			<?php if ( get_field('heading') ): ?>
+			<h2><?php the_field('heading'); ?></h2>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php 
+		$bodysize = get_field('paragraph_size');
+		
+		if ( $bodysize == 'large' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<div class="large"><?php the_field('content'); ?></div>
+			<?php endif; ?>
+			
+		<?php elseif ( $bodysize == 'small' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<?php the_field('content'); ?>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php if( have_rows('cta_button') ): ?>
+		<?php while( have_rows('cta_button') ): the_row(); ?>
+		<?php if ( get_sub_field('button_link') ): ?>
+		<footer>
+			
+			<ul class="actions center-items">
+				<li><a href="<?php the_sub_field('button_link'); ?>" class="dark button"><?php the_sub_field('button_label'); ?></a></li>
+			</ul>
+			
+		</footer>
+		<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+		
+	</header>
+	<?php elseif ( get_field('text_color') == 'light' ): ?>
+	<header class="light max-width-800">
+		
+		<?php
+		$headingsize = get_field('heading_size');
+		
+		if ( $headingsize == 'large' ): ?>
+			
+			<?php if ( get_field('heading') ): ?>
+			<h1><?php the_field('heading'); ?></h1>
+			<?php endif; ?>
+		
+		<?php elseif ( $headingsize == 'small' ): ?>
+		
+			<?php if ( get_field('heading') ): ?>
+			<h2><?php the_field('heading'); ?></h2>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php 
+		$bodysize = get_field('paragraph_size');
+		
+		if ( $bodysize == 'large' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<div class="large"><?php the_field('content'); ?></div>
+			<?php endif; ?>
+			
+		<?php elseif ( $bodysize == 'small' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<?php the_field('content'); ?>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php if( have_rows('cta_button') ): ?>
+		<?php while( have_rows('cta_button') ): the_row(); ?>
+		<?php if ( get_sub_field('button_link') ): ?>
+		<footer>
+			
+			<ul class="actions center-items">
+				<li><a href="<?php the_sub_field('button_link'); ?>" class="light button"><?php the_sub_field('button_label'); ?></a></li>
+			</ul>
+			
+		</footer>
+		<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+		
+	</header>
+	<?php endif; ?>
+
 <?php elseif ( $imageoverlay == 'light' && $backgroundimage ): ?>
 <section class="banner light-overlay" style="background-image: url(<?php the_field('background_image'); ?>);">
+
+	<?php if ( get_field('text_color') == 'dark' ): ?>
+	<header class="max-width-800">
+		
+		<?php
+		$headingsize = get_field('heading_size');
+		
+		if ( $headingsize == 'large' ): ?>
+			
+			<?php if ( get_field('heading') ): ?>
+			<h1><?php the_field('heading'); ?></h1>
+			<?php endif; ?>
+		
+		<?php elseif ( $headingsize == 'small' ): ?>
+		
+			<?php if ( get_field('heading') ): ?>
+			<h2><?php the_field('heading'); ?></h2>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php 
+		$bodysize = get_field('paragraph_size');
+		
+		if ( $bodysize == 'large' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<div class="large"><?php the_field('content'); ?></div>
+			<?php endif; ?>
+			
+		<?php elseif ( $bodysize == 'small' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<?php the_field('content'); ?>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php if( have_rows('cta_button') ): ?>
+		<?php while( have_rows('cta_button') ): the_row(); ?>
+		<?php if ( get_sub_field('button_link') ): ?>
+		<footer>
+			
+			<ul class="actions center-items">
+				<li><a href="<?php the_sub_field('button_link'); ?>" class="dark button"><?php the_sub_field('button_label'); ?></a></li>
+			</ul>
+			
+		</footer>
+		<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+		
+	</header>
+	<?php elseif ( get_field('text_color') == 'light' ): ?>
+	<header class="light max-width-800">
+		
+		<?php
+		$headingsize = get_field('heading_size');
+		
+		if ( $headingsize == 'large' ): ?>
+			
+			<?php if ( get_field('heading') ): ?>
+			<h1><?php the_field('heading'); ?></h1>
+			<?php endif; ?>
+		
+		<?php elseif ( $headingsize == 'small' ): ?>
+		
+			<?php if ( get_field('heading') ): ?>
+			<h2><?php the_field('heading'); ?></h2>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php 
+		$bodysize = get_field('paragraph_size');
+		
+		if ( $bodysize == 'large' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<div class="large"><?php the_field('content'); ?></div>
+			<?php endif; ?>
+			
+		<?php elseif ( $bodysize == 'small' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<?php the_field('content'); ?>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php if( have_rows('cta_button') ): ?>
+		<?php while( have_rows('cta_button') ): the_row(); ?>
+		<?php if ( get_sub_field('button_link') ): ?>
+		<footer>
+			
+			<ul class="actions center-items">
+				<li><a href="<?php the_sub_field('button_link'); ?>" class="light button"><?php the_sub_field('button_label'); ?></a></li>
+			</ul>
+			
+		</footer>
+		<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+		
+	</header>
+	<?php endif; ?>
 
 <?php elseif ( $backgroundimage ): ?>
 <section class="banner" style="background-image: <?php the_field('background_image'); ?>;">
 
+	<?php if ( get_field('text_color') == 'dark' ): ?>
+	<header class="max-width-800">
+		
+		<?php
+		$headingsize = get_field('heading_size');
+		
+		if ( $headingsize == 'large' ): ?>
+			
+			<?php if ( get_field('heading') ): ?>
+			<h1><?php the_field('heading'); ?></h1>
+			<?php endif; ?>
+		
+		<?php elseif ( $headingsize == 'small' ): ?>
+		
+			<?php if ( get_field('heading') ): ?>
+			<h2><?php the_field('heading'); ?></h2>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php 
+		$bodysize = get_field('paragraph_size');
+		
+		if ( $bodysize == 'large' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<div class="large"><?php the_field('content'); ?></div>
+			<?php endif; ?>
+			
+		<?php elseif ( $bodysize == 'small' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<?php the_field('content'); ?>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php if( have_rows('cta_button') ): ?>
+		<?php while( have_rows('cta_button') ): the_row(); ?>
+		<?php if ( get_sub_field('button_link') ): ?>
+		<footer>
+			
+			<ul class="actions center-items">
+				<li><a href="<?php the_sub_field('button_link'); ?>" class="dark button"><?php the_sub_field('button_label'); ?></a></li>
+			</ul>
+			
+		</footer>
+		<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+		
+	</header>
+	<?php elseif ( get_field('text_color') == 'light' ): ?>
+	<header class="light max-width-800">
+		
+		<?php
+		$headingsize = get_field('heading_size');
+		
+		if ( $headingsize == 'large' ): ?>
+			
+			<?php if ( get_field('heading') ): ?>
+			<h1><?php the_field('heading'); ?></h1>
+			<?php endif; ?>
+		
+		<?php elseif ( $headingsize == 'small' ): ?>
+		
+			<?php if ( get_field('heading') ): ?>
+			<h2><?php the_field('heading'); ?></h2>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php 
+		$bodysize = get_field('paragraph_size');
+		
+		if ( $bodysize == 'large' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<div class="large"><?php the_field('content'); ?></div>
+			<?php endif; ?>
+			
+		<?php elseif ( $bodysize == 'small' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<?php the_field('content'); ?>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php if( have_rows('cta_button') ): ?>
+		<?php while( have_rows('cta_button') ): the_row(); ?>
+		<?php if ( get_sub_field('button_link') ): ?>
+		<footer>
+			
+			<ul class="actions center-items">
+				<li><a href="<?php the_sub_field('button_link'); ?>" class="light button"><?php the_sub_field('button_label'); ?></a></li>
+			</ul>
+			
+		</footer>
+		<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+		
+	</header>
+	<?php endif; ?>
+
 <?php elseif ( $backgroundcolour ): ?>
 <section class="banner" style="background-color: <?php the_field('background_color'); ?>;">
 
+	<?php if ( get_field('text_color') == 'dark' ): ?>
+	<header class="max-width-800">
+		
+		<?php
+		$headingsize = get_field('heading_size');
+		
+		if ( $headingsize == 'large' ): ?>
+			
+			<?php if ( get_field('heading') ): ?>
+			<h1><?php the_field('heading'); ?></h1>
+			<?php endif; ?>
+		
+		<?php elseif ( $headingsize == 'small' ): ?>
+		
+			<?php if ( get_field('heading') ): ?>
+			<h2><?php the_field('heading'); ?></h2>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php 
+		$bodysize = get_field('paragraph_size');
+		
+		if ( $bodysize == 'large' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<div class="large"><?php the_field('content'); ?></div>
+			<?php endif; ?>
+			
+		<?php elseif ( $bodysize == 'small' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<?php the_field('content'); ?>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php if( have_rows('cta_button') ): ?>
+		<?php while( have_rows('cta_button') ): the_row(); ?>
+		<?php if ( get_sub_field('button_link') ): ?>
+		<footer>
+			
+			<ul class="actions center-items">
+				<li><a href="<?php the_sub_field('button_link'); ?>" class="dark button"><?php the_sub_field('button_label'); ?></a></li>
+			</ul>
+			
+		</footer>
+		<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+		
+	</header>
+	<?php elseif ( get_field('text_color') == 'light' ): ?>
+	<header class="light max-width-800">
+		
+		<?php
+		$headingsize = get_field('heading_size');
+		
+		if ( $headingsize == 'large' ): ?>
+			
+			<?php if ( get_field('heading') ): ?>
+			<h1><?php the_field('heading'); ?></h1>
+			<?php endif; ?>
+		
+		<?php elseif ( $headingsize == 'small' ): ?>
+		
+			<?php if ( get_field('heading') ): ?>
+			<h2><?php the_field('heading'); ?></h2>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php 
+		$bodysize = get_field('paragraph_size');
+		
+		if ( $bodysize == 'large' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<div class="large"><?php the_field('content'); ?></div>
+			<?php endif; ?>
+			
+		<?php elseif ( $bodysize == 'small' ): ?>
+		
+			<?php if ( get_field('content') ): ?>
+			<?php the_field('content'); ?>
+			<?php endif; ?>
+			
+		<?php endif; ?>
+		
+		<?php if( have_rows('cta_button') ): ?>
+		<?php while( have_rows('cta_button') ): the_row(); ?>
+		<?php if ( get_sub_field('button_link') ): ?>
+		<footer>
+			
+			<ul class="actions center-items">
+				<li><a href="<?php the_sub_field('button_link'); ?>" class="light button"><?php the_sub_field('button_label'); ?></a></li>
+			</ul>
+			
+		</footer>
+		<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+		
+	</header>
+	<?php endif; ?>
+
 <?php else : ?>
 <section class="banner">
-<?php endif; ?>
 	
 	<?php if ( get_field('text_color') == 'dark' ): ?>
 	<header class="max-width-800">
@@ -125,4 +548,5 @@ if ( $imageoverlay == 'dark' && $backgroundimage ): ?>
 	</header>
 	<?php endif; ?>
 </section>
+<?php endif; ?>
 <!-- /Banner -->
