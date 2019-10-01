5e038a5a86c4b7fc2b477709692741ef3fe0393a
Matt <matt@mspacecreative.com>
2019-10-01 14:35:17 -0300

    c

--- wp-content/themes/volta/includes/loops/loop-residents.php
+++ wp-content/themes/volta/includes/loops/loop-residents.php
@@ -1,9 +1,9 @@
-<div class="residents-container">
+<div class="residents-container display-flex">
 <?php 
 $loop = new WP_Query( array( 'post_type' => 'residents', 'posts_per_page' => -1 ) );
     if ( $loop->have_posts() ) :
         while ( $loop->have_posts() ) : $loop->the_post(); ?>
-		<div class="resident-container">
+		<div class="resident-container three_column">
 			<div class="grid-inner">
 				<?php if ( has_post_thumbnail() ) { ?>
 				    <?php echo the_post_thumbnail(); ?>
