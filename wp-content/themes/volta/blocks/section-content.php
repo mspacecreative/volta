7b4fdbf0977669d84771947dd4f54e9c60392c3e
Matt <matt@mspacecreative.com>
2019-10-02 13:13:57 -0300

    c

--- wp-content/themes/volta/blocks/section-content.php
+++ wp-content/themes/volta/blocks/section-content.php
@@ -2,53 +2,31 @@
 
 	if ( get_field('background_image_overlay') == 'dark' ): ?>
 	<div class="bg-img-cover dark-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
-	
 		<?php get_template_part('inc/section-content-loop'); ?>
+	</div>
 	
 	<?php elseif ( get_field('background_image_overlay') == 'light' ): ?>
-	<div class="white-bg bg-img-cover light-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
-	
+	<div class="bg-img-cover light-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
 		<?php get_template_part('inc/section-content-loop'); ?>
+	</div>
 	
 	<?php else : ?>
-	<div class="bg-img-cover light-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
-	
+	<div class="bg-img-cover" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
 		<?php get_template_part('inc/section-content-loop'); ?>
+	</div>
 		
 	<?php endif; ?>
 
 <?php elseif ( get_field('background_color_background_color') ): ?>
-<div class="white-bg" style="background-color: <?php the_field('background_color_background_color'); ?>;">
 	
-	<?php if ( get_field('background_image_overlay') == 'dark' ): ?>
-	<div class="bg-img-cover dark-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
-	
+	<div class="white-bg" style="background-color: <?php the_field('background_color_background_color'); ?>;">
 		<?php get_template_part('inc/section-content-loop'); ?>
-	
-	<?php elseif ( get_field('background_image_overlay') == 'light' ): ?>
-	<div class="white-bg bg-img-cover light-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
-	
-		<?php get_template_part('inc/section-content-loop'); ?>
-	
-	<?php else : ?>
-	<div class="bg-img-cover" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
-	
-		<?php get_template_part('inc/section-content-loop'); ?>
-		
-	<?php endif; ?>
+	</div>
 	
 <?php else : ?>
 	
-	<?php if ( get_field('background_image_overlay') == 'dark' ): ?>
-	<div class="bg-img-cover dark-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
-	
-		<?php get_template_part('inc/section-content-loop'); ?>
-	
-	<?php else : ?>
 	<div class="white-bg">
-	
 		<?php get_template_part('inc/section-content-loop'); ?>
-	
-	<?php endif; ?>
+	</div>
 
 <?php endif; ?>
