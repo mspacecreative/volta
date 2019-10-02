73b9bae477eaa068e9af340d2785824fa817c949
Matt <matt@mspacecreative.com>
2019-10-02 17:28:22 -0300

    c

--- wp-content/themes/volta/assets/js/main.js
+++ wp-content/themes/volta/assets/js/main.js
@@ -6,8 +6,9 @@
 
 (function($) {
 	
-	function calcNegativeMargins() {
-		if ( $('.row.content-bottom').css('padding-top') == '25em' ) {
+	
+	if ( $('.row.content-bottom').css('padding-top') == '25em' ) {
+		function calcNegativeMargins() {
 			var windowWidth = $(window).width() - 1600;
 			$('.page .features, .banner').css({
 				'margin-right' : -windowWidth,
