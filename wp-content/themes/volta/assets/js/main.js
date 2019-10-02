c53f8694d9c486ffe4799020c1c6510d76d0ec36
Matt <matt@mspacecreative.com>
2019-10-02 18:18:03 -0300

    c

--- wp-content/themes/volta/assets/js/main.js
+++ wp-content/themes/volta/assets/js/main.js
@@ -11,8 +11,8 @@
 			
 			var windowWidth = $(window).width() - 1600;
 			$('.page .features, .banner').css({
-				'margin-right' : -windowWidth / 2 + 30,
-				'margin-left' : -windowWidth / 2 + 30,
+				'margin-right' : -windowWidth + 30 / 2,
+				'margin-left' : -windowWidth + 30 / 2,
 			});
 			
 		} else {
