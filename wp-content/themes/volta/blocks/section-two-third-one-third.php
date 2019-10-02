9d24d28f6034be09cff81ecb7b2ab02c8bbdeb34
Matt <matt@mspacecreative.com>
2019-10-02 06:26:43 -0300

    c

--- wp-content/themes/volta/blocks/section-two-third-one-third.php
+++ wp-content/themes/volta/blocks/section-two-third-one-third.php
@@ -505,6 +505,300 @@ if ( $padding == 'top' && $margin == 'top' ): ?>
 	
 </div>
 
+<?php elseif ( $padding == 'bottom' ): ?>
+<div class="clear bottom-padding">
+	
+	<?php if ( get_field('heading') ): ?>
+	<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+	<?php endif; ?>
+	
+	<div class="two_third_col">
+		<?php the_field('two_third_column'); ?>
+	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php 
+			$image = get_sub_field('image_upload');
+			$size = 'large';
+			
+			if( $image ) {
+				echo wp_get_attachment_image( $image, $size );
+			}
+			?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
+</div>
+
+<?php elseif ( $padding == 'top' ): ?>
+<div class="clear top-padding">
+	
+	<?php if ( get_field('heading') ): ?>
+	<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+	<?php endif; ?>
+	
+	<div class="two_third_col">
+		<?php the_field('two_third_column'); ?>
+	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php 
+			$image = get_sub_field('image_upload');
+			$size = 'large';
+			
+			if( $image ) {
+				echo wp_get_attachment_image( $image, $size );
+			}
+			?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
+</div>
+
+<?php elseif ( $padding == 'both' ): ?>
+<div class="clear top-bottom-padding">
+	
+	<?php if ( get_field('heading') ): ?>
+	<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+	<?php endif; ?>
+	
+	<div class="two_third_col">
+		<?php the_field('two_third_column'); ?>
+	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php 
+			$image = get_sub_field('image_upload');
+			$size = 'large';
+			
+			if( $image ) {
+				echo wp_get_attachment_image( $image, $size );
+			}
+			?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
+</div>
+
+<?php elseif ( $padding == 'full' ): ?>
+<div class="clear full-padding">
+	
+	<?php if ( get_field('heading') ): ?>
+	<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+	<?php endif; ?>
+	
+	<div class="two_third_col">
+		<?php the_field('two_third_column'); ?>
+	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php 
+			$image = get_sub_field('image_upload');
+			$size = 'large';
+			
+			if( $image ) {
+				echo wp_get_attachment_image( $image, $size );
+			}
+			?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
+</div>
+
+<?php elseif ( $margin == 'bottom' ): ?>
+<div class="clear bottom-margin">
+	
+	<?php if ( get_field('heading') ): ?>
+	<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+	<?php endif; ?>
+	
+	<div class="two_third_col">
+		<?php the_field('two_third_column'); ?>
+	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php 
+			$image = get_sub_field('image_upload');
+			$size = 'large';
+			
+			if( $image ) {
+				echo wp_get_attachment_image( $image, $size );
+			}
+			?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
+</div>
+
+<?php elseif ( $margin == 'top' ): ?>
+<div class="clear top-margin">
+	
+	<?php if ( get_field('heading') ): ?>
+	<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+	<?php endif; ?>
+	
+	<div class="two_third_col">
+		<?php the_field('two_third_column'); ?>
+	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php 
+			$image = get_sub_field('image_upload');
+			$size = 'large';
+			
+			if( $image ) {
+				echo wp_get_attachment_image( $image, $size );
+			}
+			?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
+</div>
+
+<?php elseif ( $margin == 'both' ): ?>
+<div class="clear top-bottom-margin">
+	
+	<?php if ( get_field('heading') ): ?>
+	<h2 class="max-width-60"><?php the_field('heading'); ?></h2>
+	<?php endif; ?>
+	
+	<div class="two_third_col">
+		<?php the_field('two_third_column'); ?>
+	</div>
+	
+	<?php if( have_rows('one_third_column') ): ?>
+	<?php while( have_rows('one_third_column') ): the_row(); ?>
+	
+		<?php if ( get_sub_field('content_type') == 'text' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('text_editor'); ?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'image' ): ?>
+		<div class="one_third_col">
+			<?php 
+			$image = get_sub_field('image_upload');
+			$size = 'large';
+			
+			if( $image ) {
+				echo wp_get_attachment_image( $image, $size );
+			}
+			?>
+		</div>
+		
+		<?php elseif ( get_sub_field('content_type') == 'video' ): ?>
+		<div class="one_third_col">
+			<?php the_sub_field('image_upload'); ?>
+		</div>
+		<?php endif; ?>
+	
+	<?php endwhile; ?>
+	<?php endif; ?>
+	
+</div>
+
 <?php else : ?>
 <div class="clear">
 	
