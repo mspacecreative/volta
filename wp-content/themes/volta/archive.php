15a0c75ee26187b96d061f417671a7f01f266823
Matt <matt@mspacecreative.com>
2019-10-04 10:33:16 -0300

    c

--- wp-content/themes/volta/archive.php
+++ wp-content/themes/volta/archive.php
@@ -13,15 +13,15 @@
 	<?php
 	if ( get_field('featured_image_alignment', 'options') == 'top' ): ?>
 	<?php $url = get_field('featured_image_blog', 'options' ); ?>
-	<?php echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: top center;"><div class="post-header-overlay"></div></div>'; ?>
+	<?php echo '<div class="post-header" style="background-image: url('. $url.'); background-position: top center;"><div class="post-header-overlay"></div></div>'; ?>
 	<?php elseif ( get_field('featured_image_alignment', 'options') == 'bottom' ): ?>
 	<?php $url = get_field('featured_image_blog', 'options' ); ?>
-	<?php echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: bottom center;"><div class="post-header-overlay"></div></div>'; ?>
+	<?php echo '<div class="post-header" style="background-image: url('. $url.'); background-position: bottom center;"><div class="post-header-overlay"></div></div>'; ?>
 	<?php elseif ( get_field('featured_image_alignment', 'options') == 'center' ): ?>
 	<?php $url = get_field('featured_image_blog', 'options' ); ?>
-	<?php echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: center;"><div class="post-header-overlay"></div></div>'; ?>
+	<?php echo '<div class="post-header" style="background-image: url('. $url.'); background-position: center;"><div class="post-header-overlay"></div></div>'; ?>
 	<?php else : ?>
-	<div class="post-header container" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/header.jpg'); background-position: center;"><div class="post-header-overlay"></div></div>
+	<div class="post-header" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/header.jpg'); background-position: center;"><div class="post-header-overlay"></div></div>
 	<?php endif; ?>
 	<!-- /post thumbnail -->
 	
@@ -38,27 +38,29 @@
 	</nav>
 	<!-- /Nav -->
 	
-	<div class="post-content-container container white-bg clear">
+	<div class="post-content-container white-bg clear">
 
-		<main role="main">
-			<!-- section -->
-			<section>
-				
-				<!-- post title -->
-				<div class="post-title-container">
-					<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
-				</div>
-				<!-- /post title -->
-	
-				<div class="inner">
-					<?php get_template_part('includes/loops/loop-masonry'); ?>
+		<div class="container">
+			<main role="main">
+				<!-- section -->
+				<section>
 					
-					<?php get_template_part('pagination'); ?>
-				</div>
-	
-			</section>
-			<!-- /section -->
-		</main>
+					<!-- post title -->
+					<div class="post-title-container">
+						<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+					</div>
+					<!-- /post title -->
+		
+					<div class="inner">
+						<?php get_template_part('includes/loops/loop-masonry'); ?>
+						
+						<?php get_template_part('pagination'); ?>
+					</div>
+		
+				</section>
+				<!-- /section -->
+			</main>
+		</div>
 
 	</div>
 	
