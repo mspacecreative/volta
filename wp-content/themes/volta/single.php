ccabf9354e3516613120db91db482dfb668d5ffe
Matt <matt@mspacecreative.com>
2019-10-04 10:29:19 -0300

    c

--- wp-content/themes/volta/single.php
+++ wp-content/themes/volta/single.php
@@ -55,67 +55,69 @@
 	</nav>
 	<!-- /Nav -->
 	
-	<div class="post-content-container container white-bg clear">
-	<!-- post title -->
-	<div class="post-title-container">
-		<h1><?php the_title(); ?></h1>
-		<!-- post details -->
-		<div class="post-meta-container">
-			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-			<span class="date"><?php the_time('F j, Y'); ?></span>
-			<span class="author"><?php _e( 'By', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<?php endwhile;
-			endif;
-			rewind_posts(); ?>
-		</div>
-		<!-- /post details -->
-	</div>
-	<!-- /post title -->
-	<main role="main">
-		<!-- section -->
-		<section>
-						
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-						
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			
-				<div class="post-container">
-					
-					<?php the_content(); // Dynamic Content ?>
-					
-					<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-					
-					<div class="post-meta-container">
-						<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-						
-						<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-					</div>
-					
-					<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
-					
-					<?php comments_template(); ?>
+	<div class="post-content-container white-bg clear">
+		<div class="container">
+			<!-- post title -->
+			<div class="post-title-container">
+				<h1><?php the_title(); ?></h1>
+				<!-- post details -->
+				<div class="post-meta-container">
+					<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+					<span class="date"><?php the_time('F j, Y'); ?></span>
+					<span class="author"><?php _e( 'By', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+					<?php endwhile;
+					endif;
+					rewind_posts(); ?>
 				</div>
-						
-			</article>
-			<!-- /article -->
-						
-			<?php endwhile; ?>
+				<!-- /post details -->
+			</div>
+			<!-- /post title -->
+			<main role="main">
+				<!-- section -->
+				<section>
+								
+				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+								
+					<!-- article -->
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					
+						<div class="post-container">
 							
-			<?php else: ?>
+							<?php the_content(); // Dynamic Content ?>
 							
-			<!-- article -->
-			<article>
-				<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-			</article>
-			<!-- /article -->
-		
-		<?php endif; ?>
-		
-		</section>
-		<!-- /section -->
-	</main>
-	<?php get_sidebar('blog-sidebar'); ?>
+							<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+							
+							<div class="post-meta-container">
+								<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
+								
+								<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
+							</div>
+							
+							<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
+							
+							<?php comments_template(); ?>
+						</div>
+								
+					</article>
+					<!-- /article -->
+								
+					<?php endwhile; ?>
+									
+					<?php else: ?>
+									
+					<!-- article -->
+					<article>
+						<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
+					</article>
+					<!-- /article -->
+				
+				<?php endif; ?>
+				
+				</section>
+				<!-- /section -->
+			</main>
+			<?php get_sidebar('blog-sidebar'); ?>
+		</div>
 	</div>
 	
 	<?php get_footer(); ?>
