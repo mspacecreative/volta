20c0d40e532a16470b0a0792c20e5036cc7a2354
Matt <matt@mspacecreative.com>
2019-10-04 11:27:44 -0300

    c

--- wp-content/themes/volta/footer.php
+++ wp-content/themes/volta/footer.php
@@ -62,9 +62,10 @@
 							</ul>
 							<ul class="icons">
 								<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-								<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+								<li><a href="#" class="icon brands fa-facebook"><span class="label">Facebook</span></a></li>
 								<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-								<li><a href="#" class="icon brands fa-linkedin-in"><span class="label">Linkedin</span></a></li>
+								<li><a href="#" class="icon brands fa-linkedin"><span class="label">Linkedin</span></a></li>
+								<li><a href="#" class="icon brands fa-youtube"><span class="label">YouTube</span></a></li>
 							</ul>
 						</section>
 						<!-- /Contact bar -->
