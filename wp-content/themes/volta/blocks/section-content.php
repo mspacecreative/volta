6b1ef74d7751abb24c7835670a564f59b3c0b4a0
Matt <matt@mspacecreative.com>
2019-10-05 15:41:49 -0300

    c

--- wp-content/themes/volta/blocks/section-content.php
+++ wp-content/themes/volta/blocks/section-content.php
@@ -1,99 +1,133 @@
-<?php if ( get_field('background_image_background_image') ):
+<?php 
+$textcolour = get_field('text_colour_text_color');
 
-	if ( get_field('background_image_overlay') == 'dark' ): ?>
-	<div class="bg-img-cover dark-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
-		
-		<?php 
-		$textcolour = get_field('text_colour_text_color');
-		
-		if ( $textcolour = 'light' ):
-		include('inc/section-content-loop-light.php');
-		
-		elseif ( $textcolour = 'dark' ):
-		include('inc/section-content-loop.php');
-		
-		else :
-		include('inc/section-content-loop.php');
-		
-		endif; ?>
-		
-	</div>
+if ( $textcolour = 'light' ):
+
+	if ( get_field('background_image_background_image') ):
+	$textcolour = get_field('text_colour_text_color');
 	
-	<?php elseif ( get_field('background_image_overlay') == 'light' ): ?>
-	<div class="bg-img-cover light-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+		if ( get_field('background_image_overlay') == 'dark' ): ?>
+		<div class="bg-img-cover dark-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+			
+			<?php include('inc/section-content-loop-light.php'); ?>
+			
+		</div>
 		
-		<?php 
-		$textcolour = get_field('text_colour_text_color');
+		<?php elseif ( get_field('background_image_overlay') == 'light' ): ?>
+		<div class="bg-img-cover light-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+			
+			<?php include('inc/section-content-loop-light.php'); ?>
+			
+		</div>
 		
-		if ( $textcolour = 'light' ):
-		include('inc/section-content-loop-light.php');
-		
-		elseif ( $textcolour = 'dark' ):
-		include('inc/section-content-loop.php');
-		
-		else :
-		include('inc/section-content-loop.php');
-		
-		endif; ?>
-		
-	</div>
+		<?php else : ?>
+		<div class="bg-img-cover" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+			
+			<?php include('inc/section-content-loop-light.php'); ?>
+			
+		</div>
+		<?php endif; ?>
 	
+	<?php elseif ( get_field('background_color_background_color') ): ?>
+		<div class="white-bg" style="background-color: <?php the_field('background_color_background_color'); ?>;">
+			
+			<?php include('inc/section-content-loop-light.php'); ?>
+			
+		</div>
+		
 	<?php else : ?>
-	<div class="bg-img-cover" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
-		
-		<?php 
-		$textcolour = get_field('text_colour_text_color');
-		
-		if ( $textcolour = 'light' ):
-		include('inc/section-content-loop-light.php');
-		
-		elseif ( $textcolour = 'dark' ):
-		include('inc/section-content-loop.php');
-		
-		else :
-		include('inc/section-content-loop.php');
-		
-		endif; ?>
-		
-	</div>
-	<?php endif; ?>
-
-<?php elseif ( get_field('background_color_background_color') ): ?>
-	<div class="white-bg" style="background-color: <?php the_field('background_color_background_color'); ?>;">
-		
-		<?php 
-		$textcolour = get_field('text_colour_text_color');
-		
-		if ( $textcolour = 'light' ):
-		include('inc/section-content-loop-light.php');
-		
-		elseif ( $textcolour = 'dark' ):
-		include('inc/section-content-loop.php');
-		
-		else :
-		include('inc/section-content-loop.php');
-		
-		endif; ?>
-		
-	</div>
+		<div class="white-bg">
+			
+			<?php include('inc/section-content-loop-light.php'); ?>
+			
+		</div>
 	
-<?php else : ?>
-	<div class="white-bg">
-		
-		<?php 
-		$textcolour = get_field('text_colour_text_color');
-		
-		if ( $textcolour = 'light' ):
-		include('inc/section-content-loop-light.php');
-		
-		elseif ( $textcolour = 'dark' ):
-		include('inc/section-content-loop.php');
-		
-		else :
-		include('inc/section-content-loop.php');
-		
-		endif; ?>
-		
-	</div>
+	<?php endif;
+	
+elseif ( $textcolour = 'dark' ):
 
-<?php endif; ?>
+	if ( get_field('background_image_background_image') ):
+	$textcolour = get_field('text_colour_text_color');
+	
+		if ( get_field('background_image_overlay') == 'dark' ): ?>
+		<div class="bg-img-cover dark-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+			
+			<?php include('inc/section-content-loop.php'); ?>
+			
+		</div>
+		
+		<?php elseif ( get_field('background_image_overlay') == 'light' ): ?>
+		<div class="bg-img-cover light-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+			
+			<?php include('inc/section-content-loop.php'); ?>
+			
+		</div>
+		
+		<?php else : ?>
+		<div class="bg-img-cover" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+			
+			<?php include('inc/section-content-loop.php'); ?>
+			
+		</div>
+		<?php endif; ?>
+	
+	<?php elseif ( get_field('background_color_background_color') ): ?>
+		<div class="white-bg" style="background-color: <?php the_field('background_color_background_color'); ?>;">
+			
+			<?php include('inc/section-content-loop.php'); ?>
+			
+		</div>
+		
+	<?php else : ?>
+		<div class="white-bg">
+			
+			<?php include('inc/section-content-loop.php'); ?>
+			
+		</div>
+	
+	<?php endif;
+	
+else :
+
+	if ( get_field('background_image_background_image') ):
+	$textcolour = get_field('text_colour_text_color');
+	
+		if ( get_field('background_image_overlay') == 'dark' ): ?>
+		<div class="bg-img-cover dark-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+			
+			<?php include('inc/section-content-loop-light.php'); ?>
+			
+		</div>
+		
+		<?php elseif ( get_field('background_image_overlay') == 'light' ): ?>
+		<div class="bg-img-cover light-overlay" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+			
+			<?php include('inc/section-content-loop-light.php'); ?>
+			
+		</div>
+		
+		<?php else : ?>
+		<div class="bg-img-cover" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+			
+			<?php include('inc/section-content-loop-light.php'); ?>
+			
+		</div>
+		<?php endif; ?>
+	
+	<?php elseif ( get_field('background_color_background_color') ): ?>
+		<div class="white-bg" style="background-color: <?php the_field('background_color_background_color'); ?>;">
+			
+			<?php include('inc/section-content-loop-light.php'); ?>
+			
+		</div>
+		
+	<?php else : ?>
+		<div class="white-bg">
+			
+			<?php include('inc/section-content-loop-light.php'); ?>
+			
+		</div>
+	
+	<?php endif;
+	
+endif; ?>
