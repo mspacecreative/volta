b64b1996db0d97d2192389f0670d0d0f9dbb0592
Matt <matt@mspacecreative.com>
2019-10-06 16:32:31 -0300

    c

--- wp-content/themes/volta/blocks/section-three-column-layout.php
+++ wp-content/themes/volta/blocks/section-three-column-layout.php
@@ -1,7 +1,7 @@
 <article class="icon-buckets">
 	
-	<div class="shadow white-bg top-bottom-padding">
-		<div class="container row inner">
+	<div class="shadow white-bg top-bottom-padding top-bottom-margin">
+		<div class="row">
 		
 			<?php 
 			$aligncenter = get_field('align_center');
