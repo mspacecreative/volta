466a84feda2704d8f437b487163a9dc063907b83
Matt <matt@mspacecreative.com>
2019-10-07 17:54:01 -0300

    cc

--- wp-content/themes/volta/404.php
+++ wp-content/themes/volta/404.php
@@ -1,24 +1,71 @@
-<?php get_header(); ?>
+<?php
+get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<!-- Background image -->
+<div class="background">
+	<div class="gradient-overlay"></div>
+</div>
+<!-- /Background image -->
 
-			<!-- article -->
-			<article id="post-404">
 
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
+<!-- Check if thumbnail is hidden -->
+<?php if ( get_field('hide_featured_image_in_header') ): ?>
 
-			</article>
-			<!-- /article -->
+<!-- Page wrapper -->
+<div id="page-wrapper" class="no-featured-image-in-header">
+	
+<?php else : ?>
 
-		</section>
-		<!-- /section -->
-	</main>
+<!-- post thumbnail -->
+<?php
+if ( get_field('featured_image_alignment', 'options') == 'top' ): ?>
+<?php $url = get_field('featured_image_blog', 'options' ); ?>
+<?php echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: top center;"><div class="post-header-overlay"></div></div>'; ?>
+<?php elseif ( get_field('featured_image_alignment', 'options') == 'bottom' ): ?>
+<?php $url = get_field('featured_image_blog', 'options' ); ?>
+<?php echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: bottom center;"><div class="post-header-overlay"></div></div>'; ?>
+<?php elseif ( get_field('featured_image_alignment', 'options') == 'center' ): ?>
+<?php $url = get_field('featured_image_blog', 'options' ); ?>
+<?php echo '<div class="post-header container" style="background-image: url('. $url.'); background-position: center;"><div class="post-header-overlay"></div></div>'; ?>
+<?php else : ?>
+<div class="post-header container" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/header.jpg'); background-position: center;"><div class="post-header-overlay"></div></div>
+<?php endif; ?>
+<!-- /post thumbnail -->
+	
+<?php endif; ?>
+<!-- /Check if thumbnail is hidden -->
+	
+	<!-- post thumbnail on mobile -->
+	<div class="featured-image-mobile">
+		<div class="post-header-overlay"></div>
+		<?php if ( has_post_thumbnail() ): ?>
+			<?php the_post_thumbnail('featured-mobile'); ?>
+		<?php else : ?>
+			<img src="<?php echo get_template_directory_uri(); ?>/img/header-mobile.jpg">
+		<?php endif; ?>
+	</div>
+	<!-- /post thumbnail on mobile -->
+	
+	<!-- Nav -->
+	<nav id="nav">
+		<?php html5blank_nav(); ?>
+	</nav>
+	<!-- /Nav -->
+	
+	<div class="post-content-container white-bg clear">
+		<div class="post-container container">
+			<!-- post title -->
+			<div class="post-title-outer">
+				<div class="post-title-container">
+					<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
+				</div>
+			</div>
+			<!-- /post title -->
+			<div class="inner boxed">
+				<p><?php esc_html_e('Looks like this page doesn&#8217;t exist. Please refine your search, or use the navigation above.'); ?></p>
+				<p><a href="<?php echo home_url(); ?>"><?php _e( 'Return to home page', 'html5blank' ); ?></a></p>
+			</div>
+		</div>
+	</div>
 
-<?php get_sidebar(); ?>
-
-<?php get_footer(); ?>
+	<?php get_footer(); ?>
