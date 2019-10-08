a20b3153a80fdf85b249322584ce42156751e044
Matt <matt@mspacecreative.com>
2019-10-08 08:46:53 -0300

    c

--- wp-content/themes/volta/functions.php
+++ wp-content/themes/volta/functions.php
@@ -686,3 +686,23 @@ function currentResidents() {
 	return ob_get_clean();
 }
 add_shortcode( 'residents_loop', 'currentResidents' );
+
+// REMOVE PROTECTED / PRIVATE PREFIX ON PROTECTED PAGES
+function the_title_trim($title) {
+
+	$title = attribute_escape($title);
+
+	$findthese = array(
+		'#Protected:#',
+		'#Private:#'
+	);
+
+	$replacewith = array(
+		'', // What to replace "Protected:" with
+		'' // What to replace "Private:" with
+	);
+
+	$title = preg_replace($findthese, $replacewith, $title);
+	return $title;
+}
+add_filter('the_title', 'the_title_trim');
