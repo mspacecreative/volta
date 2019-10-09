c3d898efc110621a530a76d0476089e0103f7c1b
Matt <matt@mspacecreative.com>
2019-10-08 22:00:55 -0300

    c

--- wp-content/themes/volta/includes/loops/loop-blog.php
+++ wp-content/themes/volta/includes/loops/loop-blog.php
@@ -1,40 +1,43 @@
+<?php if ( have_posts() ): ?>
+
 <h1>Pages</h1>
 
-<?php while (have_posts()) : the_post(); ?>
-
-	<?php if ( $post->post_type == 'page' ) { ?>
-
-	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'clear', 'display-flex' )); ?>>
+	<?php while (have_posts()) : the_post(); ?>
+	
+		<?php if ( $post->post_type == 'page' ) { ?>
+	
+		<!-- article -->
+		<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'clear', 'display-flex' )); ?>>
+			
+			<!-- post thumbnail -->
+			<div class="one-third">
+			<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+					<?php the_post_thumbnail(array(200,200)); // Declare pixel size you need inside the array ?>
+				</a>
+			<?php endif; ?>
+			</div>
+			<!-- /post thumbnail -->
 		
-		<!-- post thumbnail -->
-		<div class="one-third">
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail(array(200,200)); // Declare pixel size you need inside the array ?>
-			</a>
-		<?php endif; ?>
-		</div>
-		<!-- /post thumbnail -->
-	
-		<!-- post title -->
-		<div class="two-third">
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<!-- /post title -->
-	
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-	
-		<?php edit_post_link(); ?>
-		</div>
-	
-	</article>
-	<!-- /article -->
-	
-	<?php } ?>
-	
-<?php endwhile; rewind_posts(); ?>
+			<!-- post title -->
+			<div class="two-third">
+			<h2>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+			</h2>
+			<!-- /post title -->
+		
+			<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+		
+			<?php edit_post_link(); ?>
+			</div>
+		
+		</article>
+		<!-- /article -->
+		
+		<?php } ?>
+		
+	<?php endwhile
+endif; rewind_posts(); ?>
 
 <?php if ( have_posts() ): ?>
 
