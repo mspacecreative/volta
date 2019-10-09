024b8b836c79b3d2cfaa25955f6682cd3d8953e6
Matt <matt@mspacecreative.com>
2019-10-08 21:43:40 -0300

    c

--- wp-content/themes/volta/includes/loops/loop-blog.php
+++ wp-content/themes/volta/includes/loops/loop-blog.php
@@ -22,13 +22,6 @@
 		</h2>
 		<!-- /post title -->
 	
-		<!-- post details -->
-		<p class="event-date">
-			<span class="date"><?php the_time('F j, Y'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-		</p>
-		<!-- /post details -->
-	
 		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
 	
 		<?php edit_post_link(); ?>
@@ -45,40 +38,41 @@
 
 	<?php if ( $post->post_type == 'post' ) { ?>
 
-	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'clear', 'display-flex' )); ?>>
-		
-		<!-- post thumbnail -->
-		<div class="one-third">
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
-			</a>
-		<?php endif; ?>
+	<div class="grid-item <?php echo $term->slug ?>">
+		<div class="grid-inner">
+			
+			<!-- post thumbnail -->
+			<?php if ( get_field('video_embed') ): ?>
+				<div class="video-container">
+					<?php the_field('video_embed'); ?>
+				</div>
+			
+			<?php else : ?>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+					<?php the_post_thumbnail('large'); // Declare pixel size you need inside the array ?>
+				</a>
+			<?php endif; ?>
+			<!-- /post thumbnail -->
+			
+			<div class="masonry-block-content">
+				
+				<!-- post details -->
+				<div class="post-meta-container">
+					<span class="date"><?php the_time('F j, Y'); ?></span>
+					<span class="author"><?php _e( 'By', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+				</div>
+				<!-- /post details -->
+				
+				<!-- post title -->
+				<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
+				<!-- /post details -->
+				
+				<?php html5wp_excerpt('html5wp_index'); ?>
+				
+			</div>
+			
 		</div>
-		<!-- /post thumbnail -->
-	
-		<!-- post title -->
-		<div class="two-third">
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<!-- /post title -->
-	
-		<!-- post details -->
-		<p class="event-date">
-			<span class="date"><?php the_time('F j, Y'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-		</p>
-		<!-- /post details -->
-	
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-	
-		<?php edit_post_link(); ?>
-		</div>
-	
-	</article>
-	<!-- /article -->
+	</div>
 	
 	<?php } ?>
 	
