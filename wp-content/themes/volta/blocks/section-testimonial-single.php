e4ca57ff3bf06cd9342b0fe5858d4b980a5c3e95
Matt <matt@mspacecreative.com>
2019-10-10 18:32:17 -0300

    c

--- wp-content/themes/volta/blocks/section-testimonial-single.php
+++ wp-content/themes/volta/blocks/section-testimonial-single.php
@@ -7,28 +7,30 @@ $loop = new WP_Query( array(
 if ( $loop->have_posts() ) :
     while ( $loop->have_posts() ) : $loop->the_post();
 
-	$post_object = get_field('testimonial');
+		$post_object = get_field('testimonial');
 	
-	if( $post_object ): 
-	
-		$post = $post_object;
-		$object_id = $post->ID;
-		setup_postdata( $post );
+		if( $post_object ): 
 		
-		if ( has_post_thumbnail() ): {
-			echo the_post_thumbnail( array(200,200) );
-		} ?>
+			$post = $post_object;
+			$object_id = $post->ID;
+			setup_postdata( $post );
+			
+			if ( has_post_thumbnail() ): {
+				echo the_post_thumbnail( array(200,200) );
+			} ?>
+			
+			<h4><?php the_title(); ?></h4>
+			
+			<p>
+				<span class="testimonial_position"><?php the_field( 'title__position', $object_id ); ?></span>
+				<?php _e(' / '); ?>
+				<span class="testimonial_company"><?php the_field( 'company', $object_id ); ?></span>
+			</p>
+			
+			<?php the_content();
 		
-		<h4><?php the_title(); ?></h4>
-		<p>
-			<span class="testimonial_position"><?php the_field('title__position', $object_id ?></span>
-			<?php _e(' / '); ?>
-			<span class="testimonial_company"><?php the_field('company', $object_id ?></span>
-		</p>
-		<?php the_content();
-		
-	wp_reset_postdata(); 
-	endif;
+		wp_reset_postdata(); 
+		endif;
 	
 	endwhile;
 endif; wp_reset_postdata(); ?>
