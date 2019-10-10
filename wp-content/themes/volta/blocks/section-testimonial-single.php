00d69920f0467a2d0454a15e9bef4570d6c25f4b
Matt <matt@mspacecreative.com>
2019-10-10 14:11:00 -0300

    c

--- wp-content/themes/volta/blocks/section-testimonial-single.php
+++ wp-content/themes/volta/blocks/section-testimonial-single.php
@@ -1,27 +1,16 @@
 <?php 
-$loop = new WP_Query( array( 
-	'post_type' => 'testimonials',
-	'posts_per_page' => 1
-	)
-);
-if ( $loop->have_posts() ) :
-    while ( $loop->have_posts() ) : $loop->the_post();
-    
-    $post_object = get_field('testimonial');
-    
-    if( $post_object ): 
-    
-    	$post = $post_object;
-    	setup_postdata( $post );
-    	$object_id = $post_object->ID;
-    	echo the_title();
-    	echo the_field( 'company', $object_id );
-    	echo the_field( 'title__position', $object_id );
-    	echo the_content();
-    	?>
-    	
-    <?php wp_reset_postdata(); 
-    endif; ?>
-    
-   	<?php endwhile;
-endif; wp_reset_postdata(); ?>
+$post_object = get_field('testimonial');
+
+if( $post_object ): 
+
+	$post = $post_object;
+	setup_postdata( $post );
+	$object_id = $post_object->ID;
+	echo the_title();
+	echo the_field( 'company', $object_id );
+	echo the_field( 'title__position', $object_id );
+	echo the_content();
+	?>
+	
+<?php wp_reset_postdata(); 
+endif; ?>
