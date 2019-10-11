ab146a60dd7e00f9a0632d7b8f5904e355eab21e
Matt <matt@mspacecreative.com>
2019-10-11 06:17:03 -0300

    c

--- wp-content/themes/volta/blocks/section-testimonial-single.php
+++ wp-content/themes/volta/blocks/section-testimonial-single.php
@@ -13,23 +13,29 @@ if ( $loop->have_posts() ) :
 		
 			$post = $post_object;
 			$object_id = $post->ID;
-			setup_postdata( $post ); 
+			setup_postdata( $post ); ?>
 			
-			if ( has_post_thumbnail() ):
-				echo the_post_thumbnail( array(200,200) );
-			endif; ?>
+			<section class="banner">
+				<article class="width-800 default-padding align-center">
 			
-			<h4><?php the_title(); ?></h4>
-			
-			<p>
-				<span class="testimonial_position"><?php the_field( 'title__position', $object_id ); ?></span>
-				<?php esc_html_e(' / '); ?>
-				<span class="testimonial_company"><?php the_field( 'company', $object_id ); ?></span>
-			</p>
-			
-			<?php the_content();
+					<?php if ( has_post_thumbnail() ):
+						echo the_post_thumbnail( array(200,200) );
+					endif; ?>
+					
+					<h4><?php the_title(); ?></h4>
+					
+					<p>
+						<span class="testimonial_position"><?php the_field( 'title__position', $object_id ); ?></span>
+						<?php esc_html_e(' / '); ?>
+						<span class="testimonial_company"><?php the_field( 'company', $object_id ); ?></span>
+					</p>
+					
+					<?php the_content(); ?>
+					
+				</article>
+			</section>
 		
-		endif;
+		<?php endif;
 		wp_reset_postdata(); ?>
 	
 	<?php endwhile;
