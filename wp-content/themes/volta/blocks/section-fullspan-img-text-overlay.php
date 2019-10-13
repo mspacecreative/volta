164e4e23eecf54a7ac55b45f57396d2589517360
Matt <matt@mspacecreative.com>
2019-10-13 15:59:03 -0300

    c

--- wp-content/themes/volta/blocks/section-fullspan-img-text-overlay.php
+++ wp-content/themes/volta/blocks/section-fullspan-img-text-overlay.php
@@ -7,6 +7,113 @@
 	<?php if( have_rows('content') ): ?>
 	<?php while( have_rows('content') ): the_row(); ?>
 		
+	<?php if ( get_sub_field('background_image_overlay') == 'dark' ): ?>
+	<section class="features shadow special white-bg dark-overlay" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
+		
+		<?php 
+		$image = get_sub_field('sponsor_branding');
+		$size = 'large';
+		if ( $image ): ?>
+		<div class="sponsor_branding">
+			<?php echo wp_get_attachment_image( $image, $size ); ?>
+		</div>
+		<?php endif; ?>
+		
+		<article class="row content-bottom">
+		
+		<?php if( have_rows('text') ): ?>
+		<?php while( have_rows('text') ): the_row(); ?>
+		
+			<?php if ( get_sub_field('alignment') == 'left' ): ?>
+			<div class="left">
+				<div class="left-side">
+					
+					<div class="full-span-content light">
+						<header>
+							<h3><?php the_sub_field('heading'); ?></h3>
+						</header>
+						<?php the_sub_field('copy'); ?>
+					</div>
+					
+				</div>
+			</div>
+		
+			<?php elseif ( get_sub_field('alignment') == 'right' ): ?>
+			<div class="right">
+				<div class="right-side clear">
+				
+					<div class="full-span-content light">
+						<header>
+							<h3><?php the_sub_field('heading'); ?></h3>
+						</header>
+						<?php the_sub_field('copy'); ?>
+					</div>
+				
+				</div>
+			</div>
+			<?php endif; ?>
+		
+		<?php endwhile;
+		endif; ?>
+		
+		</article>
+	
+	</section>
+	
+	<?php elseif ( get_sub_field('background_image_overlay') == 'light' ): ?>
+	<section class="features shadow special white-bg light-overlay" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
+		
+		<?php 
+		$image = get_sub_field('sponsor_branding');
+		$size = 'large';
+		if ( $image ): ?>
+		<div class="sponsor_branding">
+			<?php echo wp_get_attachment_image( $image, $size ); ?>
+		</div>
+		<?php endif; ?>
+		
+		<article class="row content-bottom">
+		
+		<?php if( have_rows('text') ): ?>
+		<?php while( have_rows('text') ): the_row(); ?>
+		
+			<?php if ( get_sub_field('alignment') == 'left' ): ?>
+			<div class="left">
+				<div class="left-side">
+					
+					<div class="full-span-content light">
+						<header>
+							<h3><?php the_sub_field('heading'); ?></h3>
+						</header>
+						<?php the_sub_field('copy'); ?>
+					</div>
+					
+				</div>
+			</div>
+		
+			<?php elseif ( get_sub_field('alignment') == 'right' ): ?>
+			<div class="right">
+				<div class="right-side clear">
+				
+					<div class="full-span-content light">
+						<header>
+							<h3><?php the_sub_field('heading'); ?></h3>
+						</header>
+						<?php the_sub_field('copy'); ?>
+					</div>
+				
+				</div>
+			</div>
+			<?php endif; ?>
+		
+		<?php endwhile;
+		endif; ?>
+		
+		</article>
+	
+	</section>
+	
+	<?php else : ?>
 	<section class="features shadow special white-bg" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
 		
 		<?php 
@@ -58,6 +165,7 @@
 		</article>
 	
 	</section>
+	<?php endif; ?>
 		
 	<?php endwhile;
 	endif; ?>
