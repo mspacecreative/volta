60ac68ec60976befc247a06437c9d8b6319a7845
Matt <matt@mspacecreative.com>
2019-10-14 08:53:49 -0300

    c

--- wp-content/themes/volta/page.php
+++ wp-content/themes/volta/page.php
@@ -14,6 +14,60 @@ get_header(); ?>
 <!-- Page wrapper -->
 <div id="page-wrapper" class="no-featured-image-in-header">
 	
+	<div class="post-content-container white-bg clear">
+		<div class="post-container container">
+			<!-- post title -->
+			<div class="post-title-outer">
+				<div class="post-title-container">
+					<h1><?php the_title(); ?></h1>
+				</div>
+			</div>
+			<!-- /post title -->
+			<?php if ( get_field('content_width') == 'boxed' ): ?>
+			<div class="inner boxed">
+				<?php 
+				if ( have_posts() ) {
+					while ( have_posts() ) {
+						the_post(); 
+						//
+						the_content();
+						//
+					} // end while
+				} // end if
+				?>
+			</div>
+			<?php elseif ( get_field('content_width') == 'full-width' ): ?>
+			<div class="inner">
+				<?php 
+				if ( have_posts() ) {
+					while ( have_posts() ) {
+						the_post(); 
+						//
+						the_content();
+						//
+					} // end while
+				} // end if
+				?>
+			</div>
+			<?php else : ?>
+			<div class="inner boxed">
+				<?php 
+				if ( have_posts() ) {
+					while ( have_posts() ) {
+						the_post(); 
+						//
+						the_content();
+						//
+					} // end while
+				} // end if
+				?>
+			</div>
+			<?php endif; ?>
+		</div>
+	</div>
+	
+	<?php get_footer(); ?>
+	
 <?php else : ?>
 
 <!-- Page wrapper -->
