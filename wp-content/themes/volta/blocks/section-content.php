969241c5a6626f58effc2b109ac5804c86c1048c
Matt <matt@mspacecreative.com>
2019-10-14 08:02:51 -0300

    c

--- wp-content/themes/volta/blocks/section-content.php
+++ wp-content/themes/volta/blocks/section-content.php
@@ -1,28 +1,28 @@
 <?php if ( get_field('background_image_background_image') ):
 
 	if ( get_field('background_image_overlay') == 'dark' ): ?>
-	<div class="bg-img-cover dark-overlay content-section" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+	<div id="<?php the_field('block_id'); ?>" class="bg-img-cover dark-overlay content-section" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
 		<?php include('inc/section-content-loop.php'); ?>
 	</div>
 	
 	<?php elseif ( get_field('background_image_overlay') == 'light' ): ?>
-	<div class="bg-img-cover light-overlay content-section" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+	<div id="<?php the_field('block_id'); ?>" class="bg-img-cover light-overlay content-section" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
 		<?php include('inc/section-content-loop.php'); ?>
 	</div>
 	
 	<?php else : ?>
-	<div class="bg-img-cover content-section" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
+	<div id="<?php the_field('block_id'); ?>" class="bg-img-cover content-section" style="background-image: url(<?php the_field('background_image_background_image'); ?>);">
 		<?php include('inc/section-content-loop.php'); ?>
 	</div>
 	<?php endif; ?>
 
 <?php elseif ( get_field('background_color_background_color') ): ?>
-	<div class="white-bg content-section" style="background-color: <?php the_field('background_color_background_color'); ?>;">
+	<div id="<?php the_field('block_id'); ?>" class="white-bg content-section" style="background-color: <?php the_field('background_color_background_color'); ?>;">
 		<?php include('inc/section-content-loop.php'); ?>
 	</div>
 	
 <?php else : ?>
-	<div class="white-bg content-section">
+	<div id="<?php the_field('block_id'); ?>" class="white-bg content-section">
 		<?php include('inc/section-content-loop.php'); ?>
 	</div>
 
