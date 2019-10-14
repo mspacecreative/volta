b769f36408e878f939974a6e5bb1c12f833ccbd4
Matt <matt@mspacecreative.com>
2019-10-14 08:40:24 -0300

    c

--- wp-content/themes/volta/functions.php
+++ wp-content/themes/volta/functions.php
@@ -717,9 +717,7 @@ add_filter('acf/fields/post_object/query', 'my_post_object_query', 10, 3);
 
 // FORMAT ANCHOR LINKS
 function custom_anchor_links( $value, $post_id, $field ) {
-	if ( $field['block_id'] ) {
-		$value = sanitize_title_with_dashes( $value );
-		return $value;
-	}
+	$value = sanitize_title_with_dashes( $value );
+	return $value;
 }
-add_filter('acf/format_value', 'custom_anchor_links', 10, 3);
+add_filter('acf/update_value/name=block_id', 'custom_anchor_links', 10, 3);
