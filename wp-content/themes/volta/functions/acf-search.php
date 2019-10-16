15fc988ab670d78104f3e06170e707671821f00e
Matt <matt@mspacecreative.com>
2019-10-16 08:17:31 -0300

    c

--- wp-content/themes/volta/functions/acf-search.php
+++ wp-content/themes/volta/functions/acf-search.php
@@ -1,79 +1,45 @@
 <?php
+// Make the search to index custom
 /**
- * [list_searcheable_acf list all the custom fields we want to include in our search query]
- * @return [array] [list of custom fields]
+ * Extend WordPress search to include custom fields
+ * http://adambalee.com
+ *
+ * Join posts and postmeta tables
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_join
  */
-function list_searcheable_acf(){
-  $list_searcheable_acf = array("title", "sub_title", "excerpt_short", "excerpt_long", "xyz", "myACF");
-  return $list_searcheable_acf;
-}
-/**
- * [advanced_custom_search search that encompasses ACF/advanced custom fields and taxonomies and split expression before request]
- * @param  [query-part/string]      $where    [the initial "where" part of the search query]
- * @param  [object]                 $wp_query []
- * @return [query-part/string]      $where    [the "where" part of the search query as we customized]
- * see https://vzurczak.wordpress.com/2013/06/15/extend-the-default-wordpress-search/
- * credits to Vincent Zurczak for the base query structure/spliting tags section
- */
-function advanced_custom_search( $where, &$wp_query ) {
+function cf_search_join( $join ) {
     global $wpdb;
- 
-    if ( empty( $where ))
-        return $where;
- 
-    // get search expression
-    $terms = $wp_query->query_vars[ 's' ];
-    
-    // explode search expression to get search terms
-    $exploded = explode( ' ', $terms );
-    if( $exploded === FALSE || count( $exploded ) == 0 )
-        $exploded = array( 0 => $terms );
-         
-    // reset search in order to rebuilt it as we whish
-    $where = '';
-    
-    // get searcheable_acf, a list of advanced custom fields you want to search content in
-    $list_searcheable_acf = list_searcheable_acf();
-    foreach( $exploded as $tag ) :
-        $where .= " 
-          AND (
-            (wp_posts.post_title LIKE '%$tag%')
-            OR (wp_posts.post_content LIKE '%$tag%')
-            OR EXISTS (
-              SELECT * FROM wp_postmeta
-	              WHERE post_id = wp_posts.ID
-	                AND (";
-        foreach ($list_searcheable_acf as $searcheable_acf) :
-          if ($searcheable_acf == $list_searcheable_acf[0]):
-            $where .= " (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
-          else :
-            $where .= " OR (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
-          endif;
-        endforeach;
-	        $where .= ")
-            )
-            OR EXISTS (
-              SELECT * FROM wp_comments
-              WHERE comment_post_ID = wp_posts.ID
-                AND comment_content LIKE '%$tag%'
-            )
-            OR EXISTS (
-              SELECT * FROM wp_terms
-              INNER JOIN wp_term_taxonomy
-                ON wp_term_taxonomy.term_id = wp_terms.term_id
-              INNER JOIN wp_term_relationships
-                ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
-              WHERE (
-          		taxonomy = 'post_tag'
-            		OR taxonomy = 'category'          		
-            		OR taxonomy = 'myCustomTax'
-          		)
-              	AND object_id = wp_posts.ID
-              	AND wp_terms.name LIKE '%$tag%'
-            )
-        )";
-    endforeach;
+    if ( is_search() ) {    
+        $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
+    }
+    return $join;
+}
+add_filter('posts_join', 'cf_search_join' );
+
+/**
+ * Modify the search query with posts_where
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_where
+ */
+function cf_search_where( $where ) {
+    global $pagenow, $wpdb;
+    if ( is_search() ) {
+        $where = preg_replace(
+            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+    }
     return $where;
 }
- 
-add_filter( 'posts_search', 'advanced_custom_search', 500, 2 );
+add_filter( 'posts_where', 'cf_search_where' );
+
+/**
+ * Prevent duplicates
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_distinct
+ */
+function cf_search_distinct( $where ) {
+    global $wpdb;
+    if ( is_search() ) {
+        return "DISTINCT";
+    }
+    return $where;
+}
+add_filter( 'posts_distinct', 'cf_search_distinct' );
