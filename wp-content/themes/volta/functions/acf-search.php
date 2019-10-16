28bfcdc22abd53b75de442f1ae855dbeefeaa9f3
Matt <matt@mspacecreative.com>
2019-10-16 07:48:43 -0300

    c

--- wp-content/themes/volta/functions/acf-search.php
+++ wp-content/themes/volta/functions/acf-search.php
@@ -1,34 +1,67 @@
 <?php
-add_filter( 'get_post_metadata', 'add_dynamic_post_meta', 10, 4 );
- 
-/**
- * Add dynamically-generated "post meta" to `\WP_Post` objects
- *
- * This makes it possible to access dynamic data related to a post object by simply referencing `$post->foo`.
- * That keeps the calling code much cleaner than if it were to have to do something like
- * `$foo = some_custom_logic( get_post_meta( $post->ID, 'bar', true ) ); echo esc_html( $foo )`.
- *
- * @param mixed  $value
- * @param int    $post_id
- * @param string $meta_key
- * @param int    $single   @todo handle the case where this is false
- *
- * @return mixed
- *      `null` to instruct `get_metadata()` to pull the value from the database
- *      Any non-null value will be returned as if it were pulled from the database
- */
-function add_dynamic_post_meta( $value, $post_id, $meta_key, $single ) {
-    $post = get_post( $post_id );
- 
-    if ( 'page' != $post->post_type ) {
-        return $value;
-    }
- 
-    switch ( $meta_key ) {
-        case 'verbose_page_template':
-            $value = "The page template is " . ( $post->_wp_page_template ?: 'not assigned' );
-            break;
-    }
- 
-    return $value;
+function list_searcheable_acf(){
+  $list_searcheable_acf = array("title", "sub_title", "excerpt_short", "excerpt_long", "xyz", "myACF");
+  return $list_searcheable_acf;
 }
+
+function advanced_custom_search( $where, &$wp_query ) {
+    global $wpdb;
+
+    if ( empty( $where ))
+        return $where;
+
+    // get search expression
+    $terms = $wp_query->query_vars[ 's' ];
+
+    // explode search expression to get search terms
+    $exploded = explode( ' ', $terms );
+    if( $exploded === FALSE || count( $exploded ) == 0 )
+        $exploded = array( 0 => $terms );
+
+    // reset search in order to rebuilt it as we whish
+    $where = '';
+
+    // get searcheable_acf, a list of advanced custom fields you want to search content in
+    $list_searcheable_acf = list_searcheable_acf();
+    foreach( $exploded as $tag ) :
+        $where .= " 
+          AND (
+            (wp_posts.post_title LIKE '%$tag%')
+            OR (wp_posts.post_content LIKE '%$tag%')
+            OR EXISTS (
+              SELECT * FROM wp_postmeta
+                  WHERE post_id = wp_posts.ID
+                    AND (";
+        foreach ($list_searcheable_acf as $searcheable_acf) :
+          if ($searcheable_acf == $list_searcheable_acf[0]):
+            $where .= " (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
+          else :
+            $where .= " OR (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
+          endif;
+        endforeach;
+            $where .= ")
+            )
+            OR EXISTS (
+              SELECT * FROM wp_comments
+              WHERE comment_post_ID = wp_posts.ID
+                AND comment_content LIKE '%$tag%'
+            )
+            OR EXISTS (
+              SELECT * FROM wp_terms
+              INNER JOIN wp_term_taxonomy
+                ON wp_term_taxonomy.term_id = wp_terms.term_id
+              INNER JOIN wp_term_relationships
+                ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
+              WHERE (
+                taxonomy = 'post_tag'
+                    OR taxonomy = 'category'                
+                    OR taxonomy = 'myCustomTax'
+                )
+                AND object_id = wp_posts.ID
+                AND wp_terms.name LIKE '%$tag%'
+            )
+        )";
+    endforeach;
+    return $where;
+}
+add_filter( 'posts_search', 'advanced_custom_search', 500, 2 );
