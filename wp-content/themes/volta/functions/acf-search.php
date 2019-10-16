68c79039d37ba3335aefd27885aec00850dc97c9
Matt <matt@mspacecreative.com>
2019-10-16 08:15:30 -0300

    c

--- wp-content/themes/volta/functions/acf-search.php
+++ wp-content/themes/volta/functions/acf-search.php
@@ -1,33 +1,79 @@
 <?php
-// in this case we have three ACF Relationship fields set up, they have the names
-// acf_rel_1, acf_rel_2, and acf_rel_3 (NOTE: these are the NAMES not the labels)
-// ACF stores post IDs in one form or another, so indexing them does us no good,
-// we want to index the title of the related post, so we need to extract it
-function my_searchwp_custom_fields( $customFieldValue, $customFieldName, $thePost ) {
-  // by default we're just going to send the original value back
-  $contentToIndex = $customFieldValue;
-  // check to see if this is one of the ACF Relationship fields we want to process
-  if( in_array( strtolower( $customFieldName ), array( 'two_third_column' ) ) ) {
-    // we want to index the titles, not the post IDs, so we'll wipe this out and append our titles to it
-    $contentToIndex = '';
-    // related posts are stored in an array
-    if( is_array( $customFieldValue ) ) {
-      foreach( $customFieldValue as $relatedPostData ) {
-        if( is_numeric( $relatedPostData ) ) { // if you set the Relationship to store post IDs, it's numeric
-          $title = get_the_title( $relatedPostData );
-          $contentToIndex .= $title . ' ';
-        } else { // it's an array of objects
-          $postData = maybe_unserialize( $relatedPostData );
-          if( is_array( $postData ) && !empty( $postData ) ) {
-            foreach( $postData as $postID ) {
-              $title = get_the_title( absint( $postID ) );
-              $contentToIndex .= $title . ' ';
-            }
-          }
-        }
-      }
-    }
-  }
-  return $contentToIndex;
+/**
+ * [list_searcheable_acf list all the custom fields we want to include in our search query]
+ * @return [array] [list of custom fields]
+ */
+function list_searcheable_acf(){
+  $list_searcheable_acf = array("title", "sub_title", "excerpt_short", "excerpt_long", "xyz", "myACF");
+  return $list_searcheable_acf;
 }
-add_filter( 'searchwp_custom_fields', 'my_searchwp_custom_fields', 10, 3 );
+/**
+ * [advanced_custom_search search that encompasses ACF/advanced custom fields and taxonomies and split expression before request]
+ * @param  [query-part/string]      $where    [the initial "where" part of the search query]
+ * @param  [object]                 $wp_query []
+ * @return [query-part/string]      $where    [the "where" part of the search query as we customized]
+ * see https://vzurczak.wordpress.com/2013/06/15/extend-the-default-wordpress-search/
+ * credits to Vincent Zurczak for the base query structure/spliting tags section
+ */
+function advanced_custom_search( $where, &$wp_query ) {
+    global $wpdb;
+ 
+    if ( empty( $where ))
+        return $where;
+ 
+    // get search expression
+    $terms = $wp_query->query_vars[ 's' ];
+    
+    // explode search expression to get search terms
+    $exploded = explode( ' ', $terms );
+    if( $exploded === FALSE || count( $exploded ) == 0 )
+        $exploded = array( 0 => $terms );
+         
+    // reset search in order to rebuilt it as we whish
+    $where = '';
+    
+    // get searcheable_acf, a list of advanced custom fields you want to search content in
+    $list_searcheable_acf = list_searcheable_acf();
+    foreach( $exploded as $tag ) :
+        $where .= " 
+          AND (
+            (wp_posts.post_title LIKE '%$tag%')
+            OR (wp_posts.post_content LIKE '%$tag%')
+            OR EXISTS (
+              SELECT * FROM wp_postmeta
+	              WHERE post_id = wp_posts.ID
+	                AND (";
+        foreach ($list_searcheable_acf as $searcheable_acf) :
+          if ($searcheable_acf == $list_searcheable_acf[0]):
+            $where .= " (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
+          else :
+            $where .= " OR (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
+          endif;
+        endforeach;
+	        $where .= ")
+            )
+            OR EXISTS (
+              SELECT * FROM wp_comments
+              WHERE comment_post_ID = wp_posts.ID
+                AND comment_content LIKE '%$tag%'
+            )
+            OR EXISTS (
+              SELECT * FROM wp_terms
+              INNER JOIN wp_term_taxonomy
+                ON wp_term_taxonomy.term_id = wp_terms.term_id
+              INNER JOIN wp_term_relationships
+                ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
+              WHERE (
+          		taxonomy = 'post_tag'
+            		OR taxonomy = 'category'          		
+            		OR taxonomy = 'myCustomTax'
+          		)
+              	AND object_id = wp_posts.ID
+              	AND wp_terms.name LIKE '%$tag%'
+            )
+        )";
+    endforeach;
+    return $where;
+}
+ 
+add_filter( 'posts_search', 'advanced_custom_search', 500, 2 );
