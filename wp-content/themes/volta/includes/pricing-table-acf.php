ab8f527cc82ccdbc33a43c631ee7fb8b18d5f470
Matt <matt@mspacecreative.com>
2019-10-30 16:26:57 -0300

    c

--- wp-content/themes/volta/includes/pricing-table-acf.php
+++ wp-content/themes/volta/includes/pricing-table-acf.php
@@ -225,7 +225,7 @@
 		
 		$people = get_sub_field('people');
 		
-		if ( $people && in_array('innovation', $people) ): ?>
+		if ( $people && in_array('corporate', $people) ): ?>
 		<td><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30">
 			<g>
 				<path fill="#231F20" d="M15,1.598c7.39,0,13.401,6.012,13.401,13.401S22.39,28.401,15,28.401S1.598,22.389,1.598,15
@@ -252,7 +252,7 @@
 		
 		$people = get_sub_field('people');
 		
-		if ( $people && in_array('innovation', $people) ): ?>
+		if ( $people && in_array('corporate', $people) ): ?>
 		<td><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30">
 			<g>
 				<path fill="#231F20" d="M15,1.598c7.39,0,13.401,6.012,13.401,13.401S22.39,28.401,15,28.401S1.598,22.389,1.598,15
@@ -279,7 +279,7 @@
 		
 		$people = get_sub_field('people');
 		
-		if ( $people && in_array('innovation', $people) ): ?>
+		if ( $people && in_array('corporate', $people) ): ?>
 		<td><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30">
 			<g>
 				<path fill="#231F20" d="M15,1.598c7.39,0,13.401,6.012,13.401,13.401S22.39,28.401,15,28.401S1.598,22.389,1.598,15
@@ -306,7 +306,7 @@
 		
 		$people = get_sub_field('people');
 		
-		if ( $people && in_array('innovation', $people) ): ?>
+		if ( $people && in_array('corporate', $people) ): ?>
 		<td><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30">
 			<g>
 				<path fill="#231F20" d="M15,1.598c7.39,0,13.401,6.012,13.401,13.401S22.39,28.401,15,28.401S1.598,22.389,1.598,15
