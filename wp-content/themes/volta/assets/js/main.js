1d804a864aeab23e33f0390791c06d28a1ca23ea
Matt <matt@mspacecreative.com>
2019-10-31 11:28:27 -0300

    c

--- wp-content/themes/volta/assets/js/main.js
+++ wp-content/themes/volta/assets/js/main.js
@@ -24,7 +24,7 @@
 	function corporatePricingTable() {
 		// STICKY HEADER ROW
 		var header = $('.corporate_pricing_table');
-		var headerTop = $('.corporate_pricing_table').offsetTop();
+		var headerTop = $('.corporate_pricing_table').jQuery.offset().top;
 		var viewport = $(window);
 		var bottom = headerTop + header.outerHeight(true) - 46;
 		$(window).scroll(function() {
