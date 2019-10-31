88b393417f66d9f91bc1d11c8b217ee91dede8f2
Matt <matt@mspacecreative.com>
2019-10-31 12:58:24 -0300

    c

--- wp-content/themes/volta/blocks/inc/section-content-loop.php
+++ wp-content/themes/volta/blocks/inc/section-content-loop.php
@@ -52,6 +52,54 @@ elseif ( $width && $padding == 'top' ):
 	</div>
 	<?php endif;
 	
+elseif ( $width && $padding == 'both' && $margin == 'top' ):
+	
+	if ( get_field('content_editor') ): ?>
+	<div class="width-800 top-bottom-padding top-margin">
+		
+		<?php the_field('content_editor'); ?>
+		
+		<?php if( have_rows('content_cta_button') ): ?>
+		<?php while( have_rows('content_cta_button') ): the_row(); ?>
+		<?php if ( get_sub_field('content_button_link') ): ?>
+		<footer>
+			
+			<ul class="actions">
+				<li><a href="<?php the_sub_field('content_button_link'); ?>" class="dark button"><?php the_sub_field('content_button_label'); ?></a></li>
+			</ul>
+			
+		</footer>
+		<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+		
+	</div>
+	<?php endif;
+	
+elseif ( $width && $padding == 'both' && $margin == 'top' && $textcolour == 'light' ):
+	
+	if ( get_field('content_editor') ): ?>
+	<div class="width-800 top-bottom-padding top-margin light">
+		
+		<?php the_field('content_editor'); ?>
+		
+		<?php if( have_rows('content_cta_button') ): ?>
+		<?php while( have_rows('content_cta_button') ): the_row(); ?>
+		<?php if ( get_sub_field('content_button_link') ): ?>
+		<footer>
+			
+			<ul class="actions">
+				<li><a href="<?php the_sub_field('content_button_link'); ?>" class="light button"><?php the_sub_field('content_button_label'); ?></a></li>
+			</ul>
+			
+		</footer>
+		<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+		
+	</div>
+	<?php endif;
+	
 elseif ( $width && $padding == 'top' && $textcolour == 'light' ):
 	
 	if ( get_field('content_editor') ): ?>
