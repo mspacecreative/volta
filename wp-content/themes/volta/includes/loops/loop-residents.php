080a09795a23ea1937c13bf7a49d0e6c529581c9
Matt <matt@mspacecreative.com>
2019-11-20 16:15:50 -0400

    c

--- wp-content/themes/volta/includes/loops/loop-residents.php
+++ wp-content/themes/volta/includes/loops/loop-residents.php
@@ -1,12 +1,19 @@
 <div class="residents-container display-flex">
 	<?php 
-	$loop = new WP_Query( array( 
-		'post_type' => 'residents',
-		'posts_per_page' => -1,
-		'orderby'=> 'title', 
-		'order' => 'ASC'
-		)
+	$args = array(
+	    'post_type' => 'companies',
+	    'posts_per_page'=> -1,
+	    'orderby' => 'title',
+	    'order' => 'ASC',
+	    'tax_query' => array(
+	        array(
+	            'taxonomy' => 'company_type',
+	            'field' => 'slug',
+	            'terms' => 'current-resident',
+	        )
+	    )
 	);
+	$loop = new WP_Query( $args );
     if ( $loop->have_posts() ) :
         while ( $loop->have_posts() ) : $loop->the_post(); ?>
 		<div class="resident-container three_column">
