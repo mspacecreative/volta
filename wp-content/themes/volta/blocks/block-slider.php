0fb677ded643d7f18734b12e0738aa5d2eae4378
Matt <matt@mspacecreative.com>
2019-12-08 16:26:19 -0400

    c

--- wp-content/themes/volta/blocks/block-slider.php
+++ wp-content/themes/volta/blocks/block-slider.php
@@ -22,6 +22,8 @@ if ( $loop->have_posts() ) : ?>
 			<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 			<div>
 				<?php
+				$taxonomy = $category->taxonomy;
+				$term_id = $category->term_id;
 				$logo = wp_get_attachment_image(get_field('logo_image', $taxonomy . '_' . $term_id);
 				if ( $logo ) {
 					echo '<img src="' . $logo[0] . '/>';
