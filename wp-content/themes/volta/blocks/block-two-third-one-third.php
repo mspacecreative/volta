8d5e0dded697dcd059c0594c01afc72eca0fec4c
Matt <matt@mspacecreative.com>
2019-12-09 17:45:36 -0400

    c

--- wp-content/themes/volta/blocks/block-two-third-one-third.php
+++ wp-content/themes/volta/blocks/block-two-third-one-third.php
@@ -1,9 +1,17 @@
 <?php
 $padding = get_field('section_padding');
 $margin = get_field('section_margin');
+$blockanchor = get_field('block_id');
 
-if ( $padding == 'top' && $margin == 'top' ): ?>
-<div class="clear top-padding top-margin">
+if ( $blockanchor && $padding == 'top' && $margin == 'top' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear top-padding top-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
+<?php elseif ( $blockanchor && $padding == 'bottom' && $margin == 'bottom' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear bottom-padding bottom-margin">
 	
 	<?php include('inc/two-third-one-third-loop.php'); ?>
 	
@@ -16,8 +24,22 @@ if ( $padding == 'top' && $margin == 'top' ): ?>
 	
 </div>
 
-<?php elseif ( $padding == 'both' && $margin == 'both' ): ?>
+<?php elseif ( $blockanchor && $padding == 'both' && $margin == 'both' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear top-bottom-padding top-bottom-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
+<?php elseif ( && $padding == 'both' && $margin == 'both' ): ?>
 <div class="clear top-bottom-padding top-bottom-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
+<?php elseif ( $blockanchor && $padding == 'top' && $margin == 'bottom' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear top-padding bottom-margin">
 	
 	<?php include('inc/two-third-one-third-loop.php'); ?>
 	
@@ -30,8 +52,22 @@ if ( $padding == 'top' && $margin == 'top' ): ?>
 	
 </div>
 
+<?php elseif ( $blockanchor && $padding == 'top' && $margin == 'both' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear top-padding top-bottom-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
 <?php elseif ( $padding == 'top' && $margin == 'both' ): ?>
 <div class="clear top-padding top-bottom-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
+<?php elseif ( $blockanchor && $padding == 'bottom' && $margin == 'top' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear bottom-padding top-margin">
 	
 	<?php include('inc/two-third-one-third-loop.php'); ?>
 	
@@ -44,8 +80,22 @@ if ( $padding == 'top' && $margin == 'top' ): ?>
 	
 </div>
 
+<?php elseif ( $blockanchor && $padding == 'bottom' && $margin == 'both' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear bottom-padding top-bottom-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
 <?php elseif ( $padding == 'bottom' && $margin == 'both' ): ?>
 <div class="clear bottom-padding top-bottom-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
+<?php elseif ( $blockanchor && $padding == 'both' && $margin == 'top' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear top-bottom-padding top-margin">
 	
 	<?php include('inc/two-third-one-third-loop.php'); ?>
 	
@@ -58,8 +108,22 @@ if ( $padding == 'top' && $margin == 'top' ): ?>
 	
 </div>
 
+<?php elseif ( $blockanchor && $padding == 'both' && $margin == 'bottom' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear top-bottom-padding bottom-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
 <?php elseif ( $padding == 'both' && $margin == 'bottom' ): ?>
 <div class="clear top-bottom-padding bottom-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
+<?php elseif ( $blockanchor && $padding == 'bottom' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear bottom-padding">
 	
 	<?php include('inc/two-third-one-third-loop.php'); ?>
 	
@@ -72,8 +136,22 @@ if ( $padding == 'top' && $margin == 'top' ): ?>
 	
 </div>
 
+<?php elseif ( $blockanchor && $padding == 'top' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear top-padding">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
 <?php elseif ( $padding == 'top' ): ?>
 <div class="clear top-padding">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
+<?php elseif ( $blockanchor && $padding == 'both' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear top-bottom-padding">
 	
 	<?php include('inc/two-third-one-third-loop.php'); ?>
 	
@@ -86,6 +164,13 @@ if ( $padding == 'top' && $margin == 'top' ): ?>
 	
 </div>
 
+<?php elseif ( $blockanchor && $margin == 'bottom' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear bottom-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
 <?php elseif ( $margin == 'bottom' ): ?>
 <div class="clear bottom-margin">
 	
@@ -93,8 +178,22 @@ if ( $padding == 'top' && $margin == 'top' ): ?>
 	
 </div>
 
+<?php elseif ( $blockanchor && $margin == 'top' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear top-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
 <?php elseif ( $margin == 'top' ): ?>
 <div class="clear top-margin">
+	
+	<?php include('inc/two-third-one-third-loop.php'); ?>
+	
+</div>
+
+<?php elseif ( $blockanchor && $margin == 'both' ): ?>
+<div id="<?php the_field('block_id'); ?>" class="clear top-bottom-margin">
 	
 	<?php include('inc/two-third-one-third-loop.php'); ?>
 	
