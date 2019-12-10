706a89bd1c5c44441390df548bb064c0052f271c
Matt <matt@mspacecreative.com>
2019-12-10 10:31:22 -0400

    c

--- wp-content/themes/volta/blocks/inc/section-content-loop.php
+++ wp-content/themes/volta/blocks/inc/section-content-loop.php
@@ -88,6 +88,18 @@ elseif ( $width && $padding == 'top' && $textcolour == 'light' ):
 	</div>
 	<?php endif;
 	
+elseif ( $width && $padding == 'both' && $textcolour == 'light' ):
+	
+	if ( get_field('content_editor') ): ?>
+	<div class="width-800 top-bottom-padding light clear">
+		
+		<?php the_field('content_editor'); ?>
+		
+		<?php include('content-cta-button-light.php'); ?>
+		
+	</div>
+	<?php endif;
+	
 elseif ( $width && $padding == 'bottom' && $margin == 'top' ):
 	
 	if ( get_field('content_editor') ): ?>
@@ -236,18 +248,6 @@ elseif ( $padding == 'top' && $margin == 'top' && $textcolour == 'light' ):
 
 	if ( get_field('content_editor') ): ?>
 	<div class="top-padding top-margin light clear">
-		
-		<?php the_field('content_editor'); ?>
-		
-		<?php include('content-cta-button-light.php'); ?>
-		
-	</div>
-	<?php endif;
-	
-elseif ( $width && $padding == 'both' && $textcolour == 'light' ):
-	
-	if ( get_field('content_editor') ): ?>
-	<div class="width-800 top-bottom-padding light clear">
 		
 		<?php the_field('content_editor'); ?>
 		
