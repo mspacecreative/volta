8cd7c490583b9929fcddd12ed0acc1e837a593a7
Matt <matt@mspacecreative.com>
2019-12-10 10:25:59 -0400

    c

--- wp-content/themes/volta/blocks/inc/section-content-loop.php
+++ wp-content/themes/volta/blocks/inc/section-content-loop.php
@@ -256,6 +256,18 @@ elseif ( $width && $padding == 'both' ):
 	</div>
 	<?php endif;
 	
+elseif ( $width && $padding == 'both' && $textcolour == 'light' ):
+	
+	if ( get_field('content_editor') ): ?>
+	<div class="width-800 top-bottom-padding light clear">
+		
+		<?php the_field('content_editor'); ?>
+		
+		<?php include('content-cta-button-light.php'); ?>
+		
+	</div>
+	<?php endif;
+	
 elseif ( $textcolour == 'light' ):
 	
 	if ( get_field('content_editor') ): ?>
