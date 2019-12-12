89ce4066c9d0b2f4ab5268d173c92fbe32d982d1
Matt <matt@mspacecreative.com>
2019-12-12 16:23:36 -0400

    c

--- wp-content/themes/volta/blocks/block-image-grid.php
+++ wp-content/themes/volta/blocks/block-image-grid.php
@@ -1,12 +1,65 @@
 <?php 
 $imagerowcount = get_field('image_row_count');
-if ( $imagerowcount == 'one' ):
+$narrowcolumn = get_field('content_width');
+if ( $imagerowcount == 'one' && $narrowcolumn ):
+
+$images = get_field('image_grid');
+$size = 'large';
+
+	if ( $images ): ?>
+	<ul class="max-width-800 image-grid-container display-flex">
+		
+		<?php foreach( $images as $image ): ?>
+		<li class="image-container">
+			<?php if ( get_field( 'external_link', $image['ID'] ) ): ?>
+			<a href="<?php the_field( 'external_link', $image['ID'] ); ?>" target="_blank">
+				<img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
+			</a>
+			<?php else : ?>
+			<img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
+			<?php endif; ?>
+			<?php if ( $image['caption'] ): ?>
+			<p><?php echo $image['caption'] ?></p>
+			<?php endif; ?>
+		</li>
+		<?php endforeach; ?>
+		
+	</ul>
+	<?php endif;
+	
+elseif ( $imagerowcount == 'one' ):
 
 $images = get_field('image_grid');
 $size = 'large';
 
 	if ( $images ): ?>
 	<ul class="image-grid-container display-flex">
+		
+		<?php foreach( $images as $image ): ?>
+		<li class="image-container">
+			<?php if ( get_field( 'external_link', $image['ID'] ) ): ?>
+			<a href="<?php the_field( 'external_link', $image['ID'] ); ?>" target="_blank">
+				<img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
+			</a>
+			<?php else : ?>
+			<img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
+			<?php endif; ?>
+			<?php if ( $image['caption'] ): ?>
+			<p><?php echo $image['caption'] ?></p>
+			<?php endif; ?>
+		</li>
+		<?php endforeach; ?>
+		
+	</ul>
+	<?php endif;
+	
+elseif ( $imagerowcount == 'two' && $narrowcolumn ):
+
+$images = get_field('image_grid');
+$size = 'medium';
+
+	if ( $images ): ?>
+	<ul class="max-width-800 image-grid-container display-flex">
 		
 		<?php foreach( $images as $image ): ?>
 		<li class="image-container">
@@ -33,6 +86,32 @@ $size = 'medium';
 
 	if ( $images ): ?>
 	<ul class="image-grid-container display-flex">
+		
+		<?php foreach( $images as $image ): ?>
+		<li class="image-container">
+			<?php if ( get_field( 'external_link', $image['ID'] ) ): ?>
+			<a href="<?php the_field( 'external_link', $image['ID'] ); ?>" target="_blank">
+				<img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
+			</a>
+			<?php else : ?>
+			<img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
+			<?php endif; ?>
+			<?php if ( $image['caption'] ): ?>
+			<p><?php echo $image['caption'] ?></p>
+			<?php endif; ?>
+		</li>
+		<?php endforeach; ?>
+		
+	</ul>
+	<?php endif;
+	
+elseif ( $imagerowcount == 'three' && $narrowcolumn ):
+
+$images = get_field('image_grid');
+$size = 'medium';
+
+	if ( $images ): ?>
+	<ul class="max-width-800 image-grid-container display-flex">
 		
 		<?php foreach( $images as $image ): ?>
 		<li class="image-container">
@@ -85,6 +164,32 @@ $size = 'medium';
 
 	if ( $images ): ?>
 	<ul class="image-grid-container display-flex">
+		
+		<?php foreach( $images as $image ): ?>
+		<li class="image-container">
+			<?php if ( get_field( 'external_link', $image['ID'] ) ): ?>
+			<a href="<?php the_field( 'external_link', $image['ID'] ); ?>" target="_blank">
+				<img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
+			</a>
+			<?php else : ?>
+			<img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
+			<?php endif; ?>
+			<?php if ( $image['caption'] ): ?>
+			<p><?php echo $image['caption'] ?></p>
+			<?php endif; ?>
+		</li>
+		<?php endforeach; ?>
+		
+	</ul>
+	<?php endif;
+	
+elseif ( $imagerowcount == 'four' && $narrowcolumn ):
+
+$images = get_field('image_grid');
+$size = 'medium';
+
+	if ( $images ): ?>
+	<ul class="max-width-800 image-grid-container display-flex">
 		
 		<?php foreach( $images as $image ): ?>
 		<li class="image-container">
