c3297696b9952912b728aaf3edcd463499108e12
Matt <matt@mspacecreative.com>
2019-12-12 13:37:10 -0400

    c

--- wp-content/themes/volta/includes/loops/loop-alumni.php
+++ wp-content/themes/volta/includes/loops/loop-alumni.php
@@ -18,28 +18,30 @@
         foreach ( $loop as $post ) : setup_postdata( $post ); ?>
 		<div class="resident-container three_column">
 			<div class="grid-inner">
-				<?php
-				$imagelogo = get_field('logo_image', $post->ID);
-				$size = 'medium';
-				$logolink = get_field('logo_link', $post->ID);
-				$shrinklogo = get_field('square_logo', $post->ID);
-				if ( $imagelogo && $logolink && $shrinklogo ) : ?>
-				<a href="<?php the_field('logo_link', $post->ID); ?>" target="_blank">
+				<div class="logo-image-wrap">
+					<?php
+					$imagelogo = get_field('logo_image', $post->ID);
+					$size = 'medium';
+					$logolink = get_field('logo_link', $post->ID);
+					$shrinklogo = get_field('square_logo', $post->ID);
+					if ( $imagelogo && $logolink && $shrinklogo ) : ?>
+					<a href="<?php the_field('logo_link', $post->ID); ?>" target="_blank">
+						<span class="shrink-logo">
+							<?php echo wp_get_attachment_image( $imagelogo, $size ); ?>
+						</span>
+					</a>
+					<?php elseif ( $imagelogo && $shrinklogo ) : ?>
 					<span class="shrink-logo">
 						<?php echo wp_get_attachment_image( $imagelogo, $size ); ?>
 					</span>
-				</a>
-				<?php elseif ( $imagelogo && $shrinklogo ) : ?>
-				<span class="shrink-logo">
-					<?php echo wp_get_attachment_image( $imagelogo, $size ); ?>
-				</span>
-				<?php elseif ( $imagelogo && $logolink ) : ?>
-				<a href="<?php the_field('logo_link', $post->ID); ?>" target="_blank">
-					<?php echo wp_get_attachment_image( $imagelogo, $size ); ?>
-				</a>
-				<?php elseif ( $imagelogo ) : ?>
-					<?php echo wp_get_attachment_image( $imagelogo, $size ); ?>
-				<?php endif; ?>
+					<?php elseif ( $imagelogo && $logolink ) : ?>
+					<a href="<?php the_field('logo_link', $post->ID); ?>" target="_blank">
+						<?php echo wp_get_attachment_image( $imagelogo, $size ); ?>
+					</a>
+					<?php elseif ( $imagelogo ) : ?>
+						<?php echo wp_get_attachment_image( $imagelogo, $size ); ?>
+					<?php endif; ?>
+				</div>
 				<div class="resident-card-content">
 					<h3><?php the_title(); ?></h3>
 					<p><?php echo esc_html_e('Focus: '); the_field('focus'); ?></p>
