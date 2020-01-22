ff65fc379e0a4ea49f346524b7fce0163588f288
Matt <matt@mspacecreative.com>
2020-01-22 14:31:30 -0400

    c

--- wp-content/themes/volta/includes/loops/loop-team.php
+++ wp-content/themes/volta/includes/loops/loop-team.php
@@ -39,7 +39,7 @@ if ( $loop->have_posts() ) : ?>
 			
 			$file = get_field('file', $post->ID);
 			if ( $file ) : ?>
-			    <p><a href="<?php the_field('file', $post->ID); ?>" >Download High Res Headshot</a></p>
+			    <p><a href="<?php the_field('file', $post->ID); ?>" download>Download High Res Headshot</a></p>
 			<?php endif; ?>
 	
 		</div>
