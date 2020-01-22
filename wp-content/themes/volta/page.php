716509e503702f3527bf82328e3b86ed2e2e84b4
Matt <matt@mspacecreative.com>
2020-01-22 15:22:34 -0400

    c

--- wp-content/themes/volta/page.php
+++ wp-content/themes/volta/page.php
@@ -84,69 +84,25 @@ get_header(); ?>
 	$supertitle = get_field('super_title');
 	if ( $url && $supertitle ) {
 		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: top center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
+	} elseif ( $url ) {
+		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: top center;"><div class="post-header-overlay"></div></div>';
 	} ?>
 	<?php elseif ( get_field('featured_image_alignment') == 'bottom' ):
 	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 	$supertitle = get_field('super_title');
 	if ( $url && $supertitle ) {
 		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: bottom center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
+	} elseif ( $url ) {
+		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: bottom center;"><div class="post-header-overlay"></div></div>';
 	} ?>
 	<?php elseif ( get_field('featured_image_alignment') == 'center' ):
 	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 	$supertitle = get_field('super_title');
 	if ( $url && $supertitle ) {
 		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
-	} ?>
-	<?php elseif ( has_post_thumbnail( $post->ID ) ):
-	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-	$supertitle = get_field('super_title');
-	if ( $url && $supertitle ) {
-		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
-	} ?>
-	<?php elseif ( has_post_thumbnail( $post->ID ) ):
-	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-	if ( $url ) {
+	} elseif ( $url ) {
 		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: center;"><div class="post-header-overlay"></div></div>';
 	} ?>
-	<?php elseif ( get_field('featured_image_alignment') == 'top' ) : ?>
-	<div class="post-header default" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/header.jpg'); background-position: center;"><div class="post-header-overlay"></div>
-		<?php if ( get_field('super_title') ) : ?>
-		<div class="super-title-container">
-			<div class="default-padding super-title">
-				<h4><?php the_field('super_title'); ?></h4>
-			</div>
-		</div>
-		<?php endif; ?>
-	</div>
-	<?php elseif ( get_field('featured_image_alignment') == 'center' ) : ?>
-	<div class="post-header default" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/header.jpg'); background-position: center;"><div class="post-header-overlay"></div>
-		<?php if ( get_field('super_title') ) : ?>
-		<div class="super-title-container">
-			<div class="default-padding super-title">
-				<h4><?php the_field('super_title'); ?></h4>
-			</div>
-		</div>
-		<?php endif; ?>
-	</div>
-	<?php elseif ( get_field('featured_image_alignment') == 'bottom' ) : ?>
-	<div class="post-header default" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/header.jpg'); background-position: center;"><div class="post-header-overlay"></div>
-		<?php if ( get_field('super_title') ) : ?>
-		<div class="super-title-container">
-			<div class="default-padding super-title">
-				<h4><?php the_field('super_title'); ?></h4>
-			</div>
-		</div>
-		<?php endif; ?>
-	</div>
-	<?php elseif ( get_field('featured_image_alignment') == 'top' ) : ?>
-	<div class="post-header default" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/header.jpg'); background-position: center;"><div class="post-header-overlay"></div>
-	</div>
-	<?php elseif ( get_field('featured_image_alignment') == 'center' ) : ?>
-	<div class="post-header default" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/header.jpg'); background-position: center;"><div class="post-header-overlay"></div>
-	</div>
-	<?php elseif ( get_field('featured_image_alignment') == 'bottom' ) : ?>
-	<div class="post-header default" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/header.jpg'); background-position: center;"><div class="post-header-overlay"></div>
-	</div>
 	<?php else : ?>
 	<div class="post-header default" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/header.jpg'); background-position: center;"><div class="post-header-overlay"></div>
 	</div>
