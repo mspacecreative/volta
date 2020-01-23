266224aa52e88d4d754b446f6f63e857531f97e1
Matt <matt@mspacecreative.com>
2020-01-23 12:15:24 -0400

    c

--- wp-content/themes/volta/page.php
+++ wp-content/themes/volta/page.php
@@ -79,9 +79,10 @@ get_header(); ?>
 <div id="page-wrapper">
 	
 	<!-- post thumbnail -->
-	<?php if ( get_field('featured_image_alignment') == 'top' ):
+	<?php
 	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 	$supertitle = get_field('super_title');
+	if ( get_field('featured_image_alignment') == 'top' ):
 		if ( $url && $supertitle ) {
 			echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: top center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
 		} elseif ( $url ) {
@@ -93,8 +94,6 @@ get_header(); ?>
 			</div>';
 		} ?>
 	<?php elseif ( get_field('featured_image_alignment') == 'bottom' ):
-	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-	$supertitle = get_field('super_title');
 		if ( $url && $supertitle ) {
 			echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: bottom center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
 		} elseif ( $url ) {
@@ -106,8 +105,6 @@ get_header(); ?>
 			</div>';
 		} ?>
 	<?php elseif ( get_field('featured_image_alignment') == 'center' ):
-	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-	$supertitle = get_field('super_title');
 		if ( $url && $supertitle ) {
 			echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
 		} elseif ( $url ) {
@@ -118,9 +115,7 @@ get_header(); ?>
 			echo '<div class="post-header default" style="background-image: url(' . get_template_directory_uri() . '/img/header.jpg); background-position: center;"><div class="post-header-overlay"></div>
 			</div>';
 		} ?>
-	<?php
-	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-	elseif ( $url ) :
+	<?php elseif ( $url ) :
 		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: center;"><div class="post-header-overlay"></div></div>'; ?>
 	<?php else : ?>
 		<div class="post-header default" style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/header.jpg); background-position: center;"><div class="post-header-overlay"></div>
