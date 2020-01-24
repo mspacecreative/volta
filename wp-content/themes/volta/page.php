52035a19d03c84ca9143fae042639c30c1529360
Matt <matt@mspacecreative.com>
2020-01-24 10:05:10 -0400

    c

--- wp-content/themes/volta/page.php
+++ wp-content/themes/volta/page.php
@@ -78,50 +78,7 @@ get_header(); ?>
 <!-- Page wrapper -->
 <div id="page-wrapper">
 	
-	<!-- post thumbnail -->
-	<?php
-	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-	$supertitle = get_field('super_title');
-	if ( get_field('featured_image_alignment') == 'top' ):
-		if ( $url && $supertitle ) {
-			echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: top center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
-		} elseif ( $url ) {
-			echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: top center;"><div class="post-header-overlay"></div></div>';
-		} elseif ( $supertitle ) {
-			echo '<div class="post-header default" style="background-image: url(' . get_template_directory_uri() . '/img/header.jpg); background-position: center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
-		} else {
-			echo '<div class="post-header default" style="background-image: url(' . get_template_directory_uri() . '/img/header.jpg); background-position: center;"><div class="post-header-overlay"></div>
-			</div>';
-		} ?>
-	<?php elseif ( get_field('featured_image_alignment') == 'bottom' ):
-		if ( $url && $supertitle ) {
-			echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: bottom center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
-		} elseif ( $url ) {
-			echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: bottom center;"><div class="post-header-overlay"></div></div>';
-		} elseif ( $supertitle ) {
-			echo '<div class="post-header default" style="background-image: url(' . get_template_directory_uri() . '/img/header.jpg); background-position: center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
-		} else {
-			echo '<div class="post-header default" style="background-image: url(' . get_template_directory_uri() . '/img/header.jpg); background-position: center;"><div class="post-header-overlay"></div>
-			</div>';
-		} ?>
-	<?php elseif ( get_field('featured_image_alignment') == 'center' ):
-		if ( $url && $supertitle ) {
-			echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
-		} elseif ( $url ) {
-			echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: center;"><div class="post-header-overlay"></div></div>';
-		} elseif ( $supertitle ) {
-			echo '<div class="post-header default" style="background-image: url(' . get_template_directory_uri() . '/img/header.jpg); background-position: center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
-		} else {
-			echo '<div class="post-header default" style="background-image: url(' . get_template_directory_uri() . '/img/header.jpg); background-position: center;"><div class="post-header-overlay"></div>
-			</div>';
-		} ?>
-	<?php elseif ( $url ) :
-		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: center;"><div class="post-header-overlay"></div></div>'; ?>
-	<?php else : ?>
-		<div class="post-header default" style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/header.jpg); background-position: center;"><div class="post-header-overlay"></div>
-		</div>
-	<?php endif; ?>
-	<!-- /post thumbnail -->
+	<?php get_template_part('includes/templates/header_image-page'); ?>
 	
 	<!-- post thumbnail on mobile -->
 	<div class="featured-image-mobile">
