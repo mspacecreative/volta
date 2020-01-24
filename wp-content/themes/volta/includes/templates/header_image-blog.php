7fbf8a7aa4a623c981bd7e54ccc73c35e7e07991
Matt <matt@mspacecreative.com>
2020-01-24 09:59:12 -0400

    c

---
+++ wp-content/themes/volta/includes/templates/header_image-blog.php
@@ -0,0 +1,32 @@
+<?php
+$url = get_field('featured_image_blog', 'options' );
+$supertitle = get_field('blog_super_title', 'options' );
+if ( get_field('featured_image_alignment', 'options') == 'top' ) :
+	if ( $url && $supertitle ) {
+		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: top center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
+	} elseif ( $url ) {
+		echo '<div class="post-header" style="background-image: url('. $url.'); background-position: top center;"></div>';
+	} else {
+		echo '<div class="post-header" style="background-image: url(' . get_template_directory_uri() . '/img/header.jpg); background-position: center;"></div>';
+	}
+elseif ( get_field('featured_image_alignment', 'options') == 'bottom' ) :
+	if ( $url && $supertitle ) {
+		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: bottom center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
+	} elseif ( $url ) {
+		echo '<div class="post-header" style="background-image: url('. $url.'); background-position: bottom center;"></div>';
+	} else {
+		echo '<div class="post-header" style="background-image: url(' . get_template_directory_uri() . '/img/header.jpg); background-position: center;"></div>';
+	}
+elseif ( get_field('featured_image_alignment', 'options') == 'center' ) :
+	if ( $url && $supertitle ) {
+		echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: center;"><div class="post-header-overlay"></div><div class="super-title-container"><div class="default-padding super-title"><h4>' . $supertitle . '</h4></div></div></div>';
+	} elseif ( $url ) {
+		echo '<div class="post-header" style="background-image: url('. $url.'); background-position: center;"></div>';
+	} else {
+		echo '<div class="post-header" style="background-image: url(' . get_template_directory_uri() . '/img/header.jpg); background-position: center;"></div>';
+	}
+elseif ( $url ) :
+	echo '<div class="post-header" style="background-image: url(' . $url . '); background-position: center;"><div class="post-header-overlay"></div></div>';
+else :
+	echo '<div class="post-header" style="background-image: url(' . get_template_directory_uri() . '/img/header.jpg); background-position: center;"></div>';
+endif;
