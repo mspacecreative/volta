39554ebdf3dad05abadfd1ec0c9261318adba26f
Matt <matt@mspacecreative.com>
2020-01-24 10:00:55 -0400

    c

--- wp-content/themes/volta/category.php
+++ wp-content/themes/volta/category.php
@@ -15,21 +15,7 @@
 <!-- Page wrapper -->
 <div id="page-wrapper" class="blog">
 	
-	<!-- post thumbnail -->
-	<?php
-	if ( get_field('featured_image_alignment', 'options') == 'top' ): ?>
-	<?php $url = get_field('featured_image_blog', 'options' ); ?>
-	<?php echo '<div class="post-header" style="background-image: url('. $url.'); background-position: top center;"><div class="post-header-overlay"></div></div>'; ?>
-	<?php elseif ( get_field('featured_image_alignment', 'options') == 'bottom' ): ?>
-	<?php $url = get_field('featured_image_blog', 'options' ); ?>
-	<?php echo '<div class="post-header" style="background-image: url('. $url.'); background-position: bottom center;"><div class="post-header-overlay"></div></div>'; ?>
-	<?php elseif ( get_field('featured_image_alignment', 'options') == 'center' ): ?>
-	<?php $url = get_field('featured_image_blog', 'options' ); ?>
-	<?php echo '<div class="post-header" style="background-image: url('. $url.'); background-position: center;"><div class="post-header-overlay"></div></div>'; ?>
-	<?php else : ?>
-	<div class="post-header" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/header.jpg'); background-position: center;"><div class="post-header-overlay"></div></div>
-	<?php endif; ?>
-	<!-- /post thumbnail -->
+	<?php get_template_part('includes/templates/header_image-blog'); ?>
 	
 	<!-- post thumbnail on mobile -->
 	<div class="featured-image-mobile">
