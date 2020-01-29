1760d84c7efbbc0d92615f4aaf4cdd6b398d059f
Matt <matt@mspacecreative.com>
2020-01-29 17:40:54 -0400

    c

--- wp-content/themes/volta/blocks/block-testimonial-single.php
+++ wp-content/themes/volta/blocks/block-testimonial-single.php
@@ -1,42 +1,43 @@
-<?php
+<!--<?php
 $loop = new WP_Query( array( 
 	'post_type' => 'testimonials',
 	'posts_per_page' => -1,
 	)
 );
-if ( $loop->have_posts() ) : ?>
+if ( $loop->have_posts() ) : ?>-->
 			
 	<section class="banner testimonial_container light-grey-bg">
 		<article class="width-800 default-padding align-center">
 			
-			<?php
-			while ( $loop->have_posts() ) : $loop->the_post(); 
+			<!--<?php
+			while ( $loop->have_posts() ) : $loop->the_post(); -->
 			
-			$post_objects = get_field('testimonial');
+			<?php $post_objects = get_field('testimonial');
 			
 			if ( $post_objects ): ?>
 	
 			<div class="carousel">
 			
-				<?php foreach ( $post_objects as $post ) : ?>
+				<?php foreach ( $post_objects as $post ) :
+				setup_postdata( $post ); ?>
 				<div>
 				
 				<?php if ( has_post_thumbnail() ):
-					echo get_the_post_thumbnail( $post->ID, array(200,200) );
+					echo get_the_post_thumbnail( array(200,200) );
 				endif; ?>
 				
-				<h4><?php echo esc_html( get_the_title( $post->ID ) ); ?></h4>
+				<h4><?php echo esc_html( get_the_title() ); ?></h4>
 				
 				<p style="margin-bottom: 1em;">
-					<span class="testimonial_position"><?php the_field( 'title__position', $post->ID ); ?></span>
+					<span class="testimonial_position"><?php the_field( 'title__position'); ?></span>
 					
 					<?php esc_html_e(' / ');
 					
-					if ( have_rows('company_details', $post->ID ) ): 
-					while ( have_rows('company_details', $post->ID ) ): the_row();
+					if ( have_rows('company_details') ): 
+					while ( have_rows('company_details') ): the_row();
 					
-					$companyname = get_sub_field('company', $post->ID );
-					$sitelink = get_sub_field('website_link', $post->ID );
+					$companyname = get_sub_field('company');
+					$sitelink = get_sub_field('website_link');
 					
 					if ( $companyname && $sitelink ) {
 						echo '<span class="testimonial_company"><a href="' . $sitelink . '" target="_blank">' . $companyname . '</a></span>';
@@ -57,11 +58,10 @@ if ( $loop->have_posts() ) : ?>
 			
 			</div>
 			
-			<?php endif;
-			
-			endwhile; ?>
+			<?php wp_reset_postdata();
+			endif; ?>
 			
 		</article>
 	</section>
 		
-<?php endif; ?>
+<!--<?php endif; ?>-->
