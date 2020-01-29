dd961cf2381f0dc2ddb8e3148d06a9cf75fa3e62
Matt <matt@mspacecreative.com>
2020-01-29 16:22:12 -0400

    c

--- wp-content/themes/volta/blocks/block-testimonial-single.php
+++ wp-content/themes/volta/blocks/block-testimonial-single.php
@@ -14,48 +14,51 @@ if ( $loop->have_posts() ) :
 			<section class="banner testimonial_container light-grey-bg">
 				<article class="width-800 default-padding align-center">
 			
-					<?php foreach ( $post_objects as $post ) :
+					<div class="carousel">
 					
-					setup_postdata( $post );
-					
-					if ( has_post_thumbnail() ):
-						echo get_the_post_thumbnail( array(200,200) );
-					endif; ?>
-					
-					<h4><?php echo esc_html( get_the_title() ); ?></h4>
-					
-					<p style="margin-bottom: 1em;">
-						<span class="testimonial_position"><?php the_field( 'title__position'); ?></span>
+						<?php foreach ( $post_objects as $post ) : ?>
+						<div>
 						
-						<?php esc_html_e(' / ');
-						
-						if ( have_rows('company_details') ): 
-						while ( have_rows('company_details') ): the_row();
-						
-						$companyname = get_sub_field('company');
-						$sitelink = get_sub_field('website_link');
-						
-						if ( $companyname && $sitelink ) {
-							echo '<span class="testimonial_company"><a href="' . $sitelink . '" target="_blank">' . $companyname . '</a></span>';
-						} elseif ( $companyname ) {
-							echo '<span class="testimonial_company">' . $companyname . '</span>';
-						}
-						
-						endwhile;
+						<?php if ( has_post_thumbnail() ):
+							echo get_the_post_thumbnail( $object_id, array(200,200) );
 						endif; ?>
-					</p>
+						
+						<h4><?php echo esc_html( get_the_title() ); ?></h4>
+						
+						<p style="margin-bottom: 1em;">
+							<span class="testimonial_position"><?php the_field( 'title__position', $object_id ); ?></span>
+							
+							<?php esc_html_e(' / ');
+							
+							if ( have_rows('company_details', $object_id ) ): 
+							while ( have_rows('company_details', $object_id ) ): the_row();
+							
+							$companyname = get_sub_field('company', $object_id );
+							$sitelink = get_sub_field('website_link', $object_id );
+							
+							if ( $companyname && $sitelink ) {
+								echo '<span class="testimonial_company"><a href="' . $sitelink . '" target="_blank">' . $companyname . '</a></span>';
+							} elseif ( $companyname ) {
+								echo '<span class="testimonial_company">' . $companyname . '</span>';
+							}
+							
+							endwhile;
+							endif; ?>
+						</p>
+						
+						<i class="fa fa-quote-left" style="display: block; margin: 0 0 10px;"></i>
+							<span style="font-style: italic;" class="testimonial-text"><?php the_content(); ?></span>
+						<i class="fa fa-quote-right" style="display: block; margin: 15px 0 0;"></i>
+						
+						</div>
+						<?php endforeach; ?>
 					
-					<i class="fa fa-quote-left" style="display: block; margin: 0 0 10px;"></i>
-						<span style="font-style: italic;" class="testimonial-text"><?php the_content(); ?></span>
-					<i class="fa fa-quote-right" style="display: block; margin: 15px 0 0;"></i>
-					
-					<?php endforeach; ?>
+					</div>
 					
 				</article>
 			</section>
 		
 		<?php endif;
-		wp_reset_postdata();
 		
 	endwhile;
 endif; ?>
