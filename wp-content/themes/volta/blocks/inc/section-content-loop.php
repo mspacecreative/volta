1753452eda413c72ae0544624d284cbd40c2008f
Matt <matt@mspacecreative.com>
2020-01-29 14:18:40 -0400

    c

--- wp-content/themes/volta/blocks/inc/section-content-loop.php
+++ wp-content/themes/volta/blocks/inc/section-content-loop.php
@@ -53,6 +53,42 @@ elseif ( $innerpadding && $margin == 'bottom' && $textcolour == 'light' ):
 	</div>
 	<?php endif;
 	
+elseif ( $width && $padding == 'both' && $innerpadding && $textcolour == 'light' ):
+	
+	if ( get_field('content_editor') ): ?>
+	<div class="width-800 extra-top-bottom-padding light clear">
+		
+		<?php the_field('content_editor'); ?>
+		
+		<?php include('content-cta-button-light.php'); ?>
+		
+	</div>
+	<?php endif;
+	
+elseif ( $width && $padding == 'both' && $innerpadding ):
+	
+	if ( get_field('content_editor') ): ?>
+	<div class="width-800 extra-top-bottom-padding clear">
+		
+		<?php the_field('content_editor'); ?>
+		
+		<?php include('content-cta-button-dark.php'); ?>
+		
+	</div>
+	<?php endif;
+	
+elseif ( $width && $padding == 'both' && $innerpadding ):
+	
+	if ( get_field('content_editor') ): ?>
+	<div class="width-800 extra-top-bottom-padding clear">
+		
+		<?php the_field('content_editor'); ?>
+		
+		<?php include('content-cta-button-dark.php'); ?>
+		
+	</div>
+	<?php endif;
+	
 elseif ( $width && $innerpadding && $margin == 'both'):
 	
 	if ( get_field('content_editor') ): ?>
