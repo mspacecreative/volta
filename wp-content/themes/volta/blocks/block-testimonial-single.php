9e4e73ce9d858f3a860acf145778889366e76be3
Matt <matt@mspacecreative.com>
2020-01-29 17:00:02 -0400

    c

--- wp-content/themes/volta/blocks/block-testimonial-single.php
+++ wp-content/themes/volta/blocks/block-testimonial-single.php
@@ -1,61 +1,64 @@
 <?php
-$loop = get_posts( array( 
+$loop = new WP_Query( array( 
 	'post_type' => 'testimonials',
 	'posts_per_page' => -1,
 	)
 );
-if ( $loop ) :
-    
-    $post_objects = get_field('testimonial');
+if ( $loop->have_posts() ) :
+    while ( $loop->have_posts() ) : $loop->the_post(); ?>
+			
+	<section class="banner testimonial_container light-grey-bg">
+		<article class="width-800 default-padding align-center">
+			
+			<?php $post_objects = get_field('testimonial');
+			
+			if ( $post_objects ): ?>
 	
-		if ( $post_objects ): ?>
+			<div class="carousel">
 			
-			<section class="banner testimonial_container light-grey-bg">
-				<article class="width-800 default-padding align-center">
+				<?php foreach ( $post_objects as $post ) : ?>
+				<div>
+				
+				<?php if ( has_post_thumbnail() ):
+					echo get_the_post_thumbnail( $post->ID, array(200,200) );
+				endif; ?>
+				
+				<h4><?php echo esc_html( get_the_title( $post->ID ) ); ?></h4>
+				
+				<p style="margin-bottom: 1em;">
+					<span class="testimonial_position"><?php the_field( 'title__position', $post->ID ); ?></span>
+					
+					<?php esc_html_e(' / ');
+					
+					if ( have_rows('company_details', $post->ID ) ): 
+					while ( have_rows('company_details', $post->ID ) ): the_row();
+					
+					$companyname = get_sub_field('company', $post->ID );
+					$sitelink = get_sub_field('website_link', $post->ID );
+					
+					if ( $companyname && $sitelink ) {
+						echo '<span class="testimonial_company"><a href="' . $sitelink . '" target="_blank">' . $companyname . '</a></span>';
+					} elseif ( $companyname ) {
+						echo '<span class="testimonial_company">' . $companyname . '</span>';
+					}
+					
+					endwhile;
+					endif; ?>
+				</p>
+				
+				<i class="fa fa-quote-left" style="display: block; margin: 0 0 10px;"></i>
+					<span style="font-style: italic;" class="testimonial-text"><?php the_content(); ?></span>
+				<i class="fa fa-quote-right" style="display: block; margin: 15px 0 0;"></i>
+				
+				</div>
+				<?php endforeach; ?>
 			
-					<div class="carousel">
-					
-						<?php foreach ( $post_objects as $post ) : ?>
-						<div>
-						
-						<?php if ( has_post_thumbnail() ):
-							echo get_the_post_thumbnail( $post->ID, array(200,200) );
-						endif; ?>
-						
-						<h4><?php echo esc_html( get_the_title( $post->ID ) ); ?></h4>
-						
-						<p style="margin-bottom: 1em;">
-							<span class="testimonial_position"><?php the_field( 'title__position', $post->ID ); ?></span>
-							
-							<?php esc_html_e(' / ');
-							
-							if ( have_rows('company_details', $post->ID ) ): 
-							while ( have_rows('company_details', $post->ID ) ): the_row();
-							
-							$companyname = get_sub_field('company', $post->ID );
-							$sitelink = get_sub_field('website_link', $post->ID );
-							
-							if ( $companyname && $sitelink ) {
-								echo '<span class="testimonial_company"><a href="' . $sitelink . '" target="_blank">' . $companyname . '</a></span>';
-							} elseif ( $companyname ) {
-								echo '<span class="testimonial_company">' . $companyname . '</span>';
-							}
-							
-							endwhile;
-							endif; ?>
-						</p>
-						
-						<i class="fa fa-quote-left" style="display: block; margin: 0 0 10px;"></i>
-							<span style="font-style: italic;" class="testimonial-text"><?php the_content(); ?></span>
-						<i class="fa fa-quote-right" style="display: block; margin: 15px 0 0;"></i>
-						
-						</div>
-						<?php endforeach; ?>
-					
-					</div>
-					
-				</article>
-			</section>
+			</div>
+			
+			<?php endif; ?>
+			
+		</article>
+	</section>
 		
-		<?php endif;
+	<?php endwhile;
 endif; ?>
