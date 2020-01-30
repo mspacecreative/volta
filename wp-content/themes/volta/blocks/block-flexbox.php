7e62e1417fdbdb9a16ef755cef4f64424eb73fe1
Matt <matt@mspacecreative.com>
2020-01-30 13:02:42 -0400

    c

--- wp-content/themes/volta/blocks/block-flexbox.php
+++ wp-content/themes/volta/blocks/block-flexbox.php
@@ -250,6 +250,130 @@ if ( $sectionpadding == 'top' && $sectionmargin == 'top' ): ?>
 	<?php endif; ?>
 </div>
 
+<?php elseif ( $sectionpadding == 'bottom' && $sectionmargin == 'both' ): ?>
+<div class="bottom-padding top-bottom-margin">
+	<?php if ( get_field('section_heading') ): ?>
+	<h2><?php the_field('section_heading'); ?></h2>
+	<?php endif; ?>
+	<?php if ( get_field('column_count') == 'two' ): ?>
+	<?php if( have_rows('flexbox') ): ?>
+	<div class="display-flex flex-wrap clear">
+		<?php while( have_rows('flexbox') ): the_row(); ?>
+		<div class="flex-box flexbox-2">
+			<div>
+				<?php if( have_rows('flexbox_content') ): ?>
+				<?php while( have_rows('flexbox_content') ): the_row(); ?>
+				
+				<h3><?php the_sub_field('heading'); ?></h3>
+				<?php the_sub_field('body'); ?>
+				
+				<?php if( have_rows('button') ): ?>
+				<?php while( have_rows('button') ): the_row(); ?>
+				
+				<?php if ( get_sub_field('link') ): ?>
+				<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
+	<?php elseif ( get_field('column_count') == 'three' ): ?>
+	<?php if( have_rows('flexbox') ): ?>
+	<div class="display-flex flex-wrap clear">
+		<?php while( have_rows('flexbox') ): the_row(); ?>
+		<div class="flex-box flexbox-3">
+			<div>
+				<?php if( have_rows('flexbox_content') ): ?>
+				<?php while( have_rows('flexbox_content') ): the_row(); ?>
+				
+				<h3><?php the_sub_field('heading'); ?></h3>
+				<?php the_sub_field('body'); ?>
+				
+				<?php if( have_rows('button') ): ?>
+				<?php while( have_rows('button') ): the_row(); ?>
+				
+				<?php if ( get_sub_field('link') ): ?>
+				<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
+	<?php elseif ( get_field('column_count') == 'four' ): ?>
+	<?php if( have_rows('flexbox') ): ?>
+	<div class="display-flex flex-wrap clear">
+		<?php while( have_rows('flexbox') ): the_row(); ?>
+		<div class="flex-box flexbox-4">
+			<div>
+				<?php if( have_rows('flexbox_content') ): ?>
+				<?php while( have_rows('flexbox_content') ): the_row(); ?>
+				
+				<h3><?php the_sub_field('heading'); ?></h3>
+				<?php the_sub_field('body'); ?>
+				
+				<?php if( have_rows('button') ): ?>
+				<?php while( have_rows('button') ): the_row(); ?>
+				
+				<?php if ( get_sub_field('link') ): ?>
+				<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
+	<?php elseif ( get_field('column_count') == 'five' ): ?>
+	<?php if( have_rows('flexbox') ): ?>
+	<div class="display-flex flex-wrap clear">
+		<?php while( have_rows('flexbox') ): the_row(); ?>
+		<div class="flex-box flexbox-5">
+			<div>
+				<?php if( have_rows('flexbox_content') ): ?>
+				<?php while( have_rows('flexbox_content') ): the_row(); ?>
+				
+				<h3><?php the_sub_field('heading'); ?></h3>
+				<?php the_sub_field('body'); ?>
+				
+				<?php if( have_rows('button') ): ?>
+				<?php while( have_rows('button') ): the_row(); ?>
+				
+				<?php if ( get_sub_field('link') ): ?>
+				<a href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+				
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
+	<?php endif; ?>
+</div>
+
 <?php elseif ( $sectionpadding == 'both' && $sectionmargin == 'both' ): ?>
 <div class="top-bottom-padding top-bottom-margin">
 	<?php if ( get_field('section_heading') ): ?>
