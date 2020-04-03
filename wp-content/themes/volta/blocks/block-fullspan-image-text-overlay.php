bdb17a6ef7b346de8f4b4429ec6ff5d57008789a
Matt <matt@mspacecreative.com>
2020-04-03 10:51:04 -0300

    c

--- wp-content/themes/volta/blocks/block-fullspan-image-text-overlay.php
+++ wp-content/themes/volta/blocks/block-fullspan-image-text-overlay.php
@@ -10,6 +10,13 @@
 	<?php if ( get_sub_field('background_image_overlay') == 'dark' ): ?>
 	<section class="features shadow special white-bg dark-overlay" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
 		
+	<?php elseif ( get_sub_field('background_image_overlay') == 'light' ): ?>
+	<section class="features shadow special white-bg light-overlay" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
+		
+	<?php else : ?>
+	<section class="features shadow special white-bg" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
+	<?php endif; ?>
+		
 		<article class="full-img-row row-width-1280 content-bottom">
 		
 		<?php 
@@ -26,51 +33,11 @@
 		
 			<?php if ( get_sub_field('alignment') == 'left' ): ?>
 			<div class="left">
-				<div class="full-span-content light">
-					<header>
-						<h1><?php the_sub_field('heading'); ?></h1>
-					</header>
-					<?php the_sub_field('copy'); ?>
-				</div>
-			</div>
-		
 			<?php elseif ( get_sub_field('alignment') == 'right' ): ?>
 			<div class="right">
-				<div class="full-span-content light">
-					<header>
-						<h1><?php the_sub_field('heading'); ?></h1>
-					</header>
-					<?php the_sub_field('copy'); ?>
-				</div>
-			</div>
-			<?php endif; ?>
-		
-		<?php endwhile;
-		endif; ?>
-		
-		</article>
-	
-	</section>
-	
-	<?php elseif ( get_sub_field('background_image_overlay') == 'light' ): ?>
-	<section class="features shadow special white-bg light-overlay" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
-		
-		<article class="full-img-row row-width-1280  content-bottom">
-		
-		<?php 
-		$image = get_sub_field('sponsor_branding');
-		$size = 'large';
-		if ( $image ): ?>
-		<div class="sponsor_branding">
-			<?php echo wp_get_attachment_image( $image, $size ); ?>
-		</div>
-		<?php endif; ?>
-		
-		<?php if( have_rows('text') ): ?>
-		<?php while( have_rows('text') ): the_row(); ?>
-		
-			<?php if ( get_sub_field('alignment') == 'left' ): ?>
+			<?php else : ?>
 			<div class="left">
+			<?php endif; ?>	
 				<div class="full-span-content light">
 					<header>
 						<h1><?php the_sub_field('heading'); ?></h1>
@@ -78,17 +45,6 @@
 					<?php the_sub_field('copy'); ?>
 				</div>
 			</div>
-		
-			<?php elseif ( get_sub_field('alignment') == 'right' ): ?>
-			<div class="right">
-				<div class="full-span-content light">
-					<header>
-						<h1><?php the_sub_field('heading'); ?></h1>
-					</header>
-					<?php the_sub_field('copy'); ?>
-				</div>
-			</div>
-			<?php endif; ?>
 		
 		<?php endwhile;
 		endif; ?>
@@ -96,52 +52,6 @@
 		</article>
 	
 	</section>
-	
-	<?php else : ?>
-	<section class="features shadow special white-bg" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
-		
-		<article class="full-img-row row-width-1280  content-bottom">
-		
-		<?php 
-		$image = get_sub_field('sponsor_branding');
-		$size = 'large';
-		if ( $image ): ?>
-		<div class="sponsor_branding">
-			<?php echo wp_get_attachment_image( $image, $size ); ?>
-		</div>
-		<?php endif; ?>
-		
-		<?php if( have_rows('text') ): ?>
-		<?php while( have_rows('text') ): the_row(); ?>
-		
-			<?php if ( get_sub_field('alignment') == 'left' ): ?>
-			<div class="left">
-				<div class="full-span-content light">
-					<header>
-						<h1><?php the_sub_field('heading'); ?></h1>
-					</header>
-					<?php the_sub_field('copy'); ?>
-				</div>
-			</div>
-		
-			<?php elseif ( get_sub_field('alignment') == 'right' ): ?>
-			<div class="right">
-				<div class="full-span-content light">
-					<header>
-						<h1><?php the_sub_field('heading'); ?></h1>
-					</header>
-					<?php the_sub_field('copy'); ?>
-				</div>
-			</div>
-			<?php endif; ?>
-		
-		<?php endwhile;
-		endif; ?>
-		
-		</article>
-	
-	</section>
-	<?php endif; ?>
 		
 	<?php endwhile;
 	endif; ?>
