c7b848db1e3bc78b304801f9a6f4f378f96a35cf
Matt <matt@mspacecreative.com>
2020-04-03 11:32:15 -0300

    c

--- wp-content/themes/volta/blocks/block-fullspan-image-text-overlay.php
+++ wp-content/themes/volta/blocks/block-fullspan-image-text-overlay.php
@@ -7,14 +7,16 @@
 	<?php if( have_rows('content') ): ?>
 	<?php while( have_rows('content') ): the_row(); ?>
 		
-	<?php if ( get_sub_field('background_image_overlay') == 'dark' ): ?>
-	<section class="features shadow special white-bg dark-overlay" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
+	<?php 
+	$alignment = get_sub_field('alignment');
+	if ( $alignment == 'left' ): ?>
+	<section class="features shadow special white-bg features_overlay_left" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
 		
-	<?php elseif ( get_sub_field('background_image_overlay') == 'light' ): ?>
-	<section class="features shadow special white-bg light-overlay" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
+	<?php elseif ( $alignment == 'right' ): ?>
+	<section class="features shadow special white-bg features_overlay_right" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
 		
 	<?php else : ?>
-	<section class="features shadow special white-bg" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
+	<section class="features shadow special white-bg features_overlay_left" style="background-image: url(<?php the_sub_field('background_image'); ?>);">
 	<?php endif; ?>
 		
 		<article class="full-img-row row-width-1280 content-bottom">
@@ -31,9 +33,9 @@
 		<?php if( have_rows('text') ): ?>
 		<?php while( have_rows('text') ): the_row(); ?>
 		
-			<?php if ( get_sub_field('alignment') == 'left' ): ?>
+			<?php if ( $alignment == 'left' ): ?>
 			<div class="left">
-			<?php elseif ( get_sub_field('alignment') == 'right' ): ?>
+			<?php elseif ( $alignment == 'right' ): ?>
 			<div class="right">
 			<?php else : ?>
 			<div class="left">
