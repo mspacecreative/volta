6840bfd5a0e6a476f18a07c90f8b1ee1f13d65f6
Matt <matt@mspacecreative.com>
2020-05-03 12:53:06 -0300

    c

--- wp-content/themes/volta/footer.php
+++ wp-content/themes/volta/footer.php
@@ -35,11 +35,11 @@
 						<li>&copy; Volta. All rights reserved.</li>
 					</ul>
 					<ul class="icons">
-						<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon brands fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon brands fa-linkedin"><span class="label">Linkedin</span></a></li>
-						<li><a href="#" class="icon brands fa-youtube"><span class="label">YouTube</span></a></li>
+						<li><a href="https://twitter.com/voltaeffect" class="icon brands fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
+						<li><a href="https://www.facebook.com/voltaeffect/" class="icon brands fa-facebook"><span class="label" target="_blank">Facebook</span></a></li>
+						<li><a href="https://www.instagram.com/voltaeffect/" class="icon brands fa-instagram"><span class="label" target="_blank">Instagram</span></a></li>
+						<li><a href="https://www.linkedin.com/company/voltaeffect/" class="icon brands fa-linkedin"><span class="label" target="_blank">Linkedin</span></a></li>
+						<li><a href="https://www.youtube.com/user/voltaeffect" class="icon brands fa-youtube"><span class="label" target="_blank">YouTube</span></a></li>
 					</ul>
 				</div>
 				</div>
