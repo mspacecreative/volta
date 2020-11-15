39864612eb49ffc4d7a910181a8cede8f7868921
Matt <matt@mspacecreative.com>
2020-11-15 10:06:15 -0400

    c

--- wp-content/themes/volta/assets/js/main.js
+++ wp-content/themes/volta/assets/js/main.js
@@ -128,7 +128,7 @@
 	
 	// SMOOTH SCROLL
 	// Select all links with hashes
-	$('a[href*=#]:not([href=#])').click(function() {
+	$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
--- wp-content/themes/volta/includes/footer-blog-loop.php
+++ wp-content/themes/volta/includes/footer-blog-loop.php
@@ -1,15 +1,19 @@
-<?php query_posts( 'posts_per_page=3' ); ?>
+<?php
+$the_query = new WP_Query( array(
+    'post_type' => 'post',
+    'posts_per_page' => 3,
+));
 	
-	<ul class="divided">
-		<?php while ( have_posts() ) : the_post(); ?>
-		<li>
-			<header>
-				<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-			</header>
-			<span class="timestamp"><?php the_time('F j, Y'); ?></span>
-		</li>
-		<?php endwhile; ?>
+if ( $the_query->have_posts() ) : ?>
+<ul class="divided">
+	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+	<li>
+		<header>
+			<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</header>
+		<span class="timestamp"><?php the_time('F j, Y'); ?></span>
+	</li>
+	<?php endwhile; ?>
 		
-	</ul>
-	
-<?php wp_reset_query(); ?>
+</ul>
+<?php endif; wp_reset_query(); ?>
